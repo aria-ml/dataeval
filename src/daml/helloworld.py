94586e2a33c5b263af3e268ca01ed1c8202ea28c
Andrew Weng <andrew.weng@ariacoustics.com>
2023-07-25 15:17:20 +0000

    Get actual code coverage by python version matrix

--- src/daml/helloworld.py
+++ src/daml/helloworld.py
@@ -1,7 +1,19 @@
+import sys
+
+
 class HelloWorld:
     def __init__(self, message: str = "Hello World :p") -> None:
         self.message = message
 
-    def run(self):
-        print(self.message)
-        return self.message
+    def run(self) -> str:
+        result: str
+        if sys.version_info[:2] == (3, 10):
+            result = self.message
+        elif sys.version_info[:2] == (3, 9):
+            result = self.message
+        elif sys.version_info[:2] == (3, 8):
+            result = self.message
+        else:  # pragma: no cover
+            result = "Unsupported Python"
+        print(result)
+        return result
--- tests/test_helloworld.py
+++ tests/test_helloworld.py
@@ -5,6 +5,6 @@ from daml.helloworld import HelloWorld
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
-        test_message = "Hello World! :p"
+        test_message: str = "Hello World! :p"
         h_world = HelloWorld(message=test_message)
         self.assertEqual(h_world.run(), test_message)
