c6c7c90d692a028ab1b8d9413325f27578eaa397
Andrew Weng <andrew.weng@ariacoustics.com>
2023-11-28 21:45:59 +0000

    Add check for jatic_toolbox before exposing functions that have dependencies on it

--- src/daml/_internal/deps.py
+++ src/daml/_internal/deps.py
@@ -3,3 +3,7 @@ import importlib.util
 
 def is_alibi_detect_available():
     return importlib.util.find_spec("alibi_detect") is not None
+
+
+def is_jatic_toolbox_available():
+    return importlib.util.find_spec("jatic_toolbox") is not None
