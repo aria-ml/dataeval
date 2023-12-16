5bc5a62269f125e0a86c7ad46b2aa0b13efbca65
Andrew Weng <andrew.weng@ariacoustics.com>
2023-12-16 07:54:57 +0000

    update dependencies for maite rename

--- src/daml/_internal/deps.py
+++ src/daml/_internal/deps.py
@@ -5,5 +5,5 @@ def is_alibi_detect_available():
     return importlib.util.find_spec("alibi_detect") is not None
 
 
-def is_jatic_toolbox_available():
-    return importlib.util.find_spec("jatic_toolbox") is not None
+def is_maite_available():
+    return importlib.util.find_spec("maite") is not None
--- src/daml/interop/__init__.py
+++ src/daml/interop/__init__.py
@@ -1,8 +1,8 @@
-from daml._internal.deps import is_jatic_toolbox_available
+from daml._internal.deps import is_maite_available
 
-if is_jatic_toolbox_available():  # pragma: no cover
+if is_maite_available():  # pragma: no cover
     from . import wrappers  # noqa F401
 
     __all__ = ["wrappers"]
 
-del is_jatic_toolbox_available
+del is_maite_available
