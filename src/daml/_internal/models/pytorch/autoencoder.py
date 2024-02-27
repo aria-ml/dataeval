4a63c2b8735a74aaaacf2de9d2d543a775dab351
Robert Jullens <shaun.jullens@ariacoustics.com>
2024-02-27 00:38:12 +0000

    AE Trainer workflow notebook. AE Trainer updates based on notebook

--- src/daml/_internal/models/pytorch/autoencoder.py
+++ src/daml/_internal/models/pytorch/autoencoder.py
@@ -1,4 +1,4 @@
-from typing import Any, Union
+from typing import Any, List, Union
 
 import torch
 import torch.nn as nn
@@ -21,7 +21,7 @@ class AETrainer:
     def __init__(
         self,
         model: nn.Module,
-        device: Union[str, torch.device] = "cpu",
+        device: Union[str, torch.device] = "auto",
         batch_size: int = 8,
     ):
         """
@@ -32,12 +32,13 @@ class AETrainer:
         batch_size : int, default 8
             Number of images to group together in `torch.utils.data.DataLoader`
         """
-
+        if device == "auto":
+            device = "cuda" if torch.cuda.is_available() else "cpu"
         self.device = device
         self.model = model.to(device)
         self.batch_size = batch_size
 
-    def train(self, dataset: Dataset, epochs: int = 25):
+    def train(self, dataset: Dataset, epochs: int = 25) -> List[float]:
         """
         Basic training function for Autoencoder models for reconstruction tasks
 
@@ -55,24 +56,45 @@ class AETrainer:
         To replace this function with a custom function, do
             AETrainer.train = custom_function
         """
+        # Setup training
         self.model.train()
-        dl = DataLoader(dataset, batch_size=self.batch_size)
+        dataloader = DataLoader(dataset, batch_size=self.batch_size)
         opt = Adam(self.model.parameters(), lr=0.001)
         criterion = nn.MSELoss().to(self.device)
-        loss = 0
+        # Record loss
+        loss_history: List[float] = []
+        batch_idx: int = 0
+
         for _ in range(epochs):
-            for batch in dl:
+            epoch_loss: float = 0
+            for batch_idx, batch in enumerate(dataloader):
                 imgs = get_images_from_batch(batch)
                 imgs = imgs.to(self.device)
+                # Zero your gradients for every batch!
                 opt.zero_grad()
+
+                # Make predictions for this batch
                 pred = self.model(imgs)
+
+                # Compute the loss and its gradients
                 loss = criterion(pred, imgs)
                 loss.backward()
+
+                # Adjust learning weights
                 opt.step()
 
+                # Gather data and report
+                epoch_loss += loss.item()
+            # Will take the average from all batches
+            epoch_loss /= batch_idx + 1
+            loss_history.append(epoch_loss)
+
+        return loss_history
+
+    @torch.no_grad
     def eval(self, dataset: Dataset) -> float:
         """
-        Basic training function for Autoencoder models for reconstruction tasks
+        Basic evaluation function for Autoencoder models for reconstruction tasks
 
         Uses `torch.optim.Adam` and `torch.nn.MSELoss` as default hyperparameters
 
@@ -92,18 +114,20 @@ class AETrainer:
             AETrainer.eval = custom_function
         """
         self.model.eval()
-        dl = DataLoader(dataset, batch_size=self.batch_size)
+        dataloader = DataLoader(dataset, batch_size=self.batch_size)
         criterion = nn.MSELoss().to(self.device)
         total_loss: float = 0.0
-        with torch.no_grad():
-            for batch in dl:
-                imgs = get_images_from_batch(batch)
-                imgs = imgs.to(self.device)
-                pred = self.model(imgs)
-                loss = criterion(pred, imgs)
-                total_loss += loss
-        return total_loss
+        batch_idx: int = 0
 
+        for batch_idx, batch in enumerate(dataloader):
+            imgs = get_images_from_batch(batch)
+            imgs = imgs.to(self.device)
+            pred = self.model(imgs)
+            loss = criterion(pred, imgs)
+            total_loss += loss.item()
+        return total_loss / (batch_idx + 1)
+
+    @torch.no_grad
     def encode(self, dataset: Dataset) -> torch.Tensor:
         """
         Encode data through model if it has an encode attribute,
@@ -123,19 +147,20 @@ class AETrainer:
         dl = DataLoader(dataset, batch_size=self.batch_size)
         encodings = torch.Tensor([])
 
+        # Get encode function if defined
         if getattr(self.model, "encode", None) is not None:
             encode_func = self.model.encode
         else:
             encode_func = self.model.forward
-        with torch.no_grad():
-            for batch in dl:
-                imgs = get_images_from_batch(batch)
-                imgs = imgs.to(self.device)
-                embeddings = encode_func(imgs)
-                if len(encodings):
-                    encodings = torch.vstack((encodings, embeddings))
-                else:
-                    encodings = embeddings
+        # Accumulate encodings from batches
+        for batch in dl:
+            imgs = get_images_from_batch(batch)
+            imgs = imgs.to(self.device)
+            embeddings = encode_func(imgs).to("cpu")
+            if len(encodings):
+                encodings = torch.vstack((encodings, embeddings))
+            else:
+                encodings = embeddings
 
         return encodings
 
