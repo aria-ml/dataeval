8458d26362b9a6a1fbe9fa96261efa795b22656f
Andrew Weng <andrew.weng@ariacoustics.com>
2024-03-19 20:39:43 +0000

    Update jupyter-cache in scheduled release pipeline

--- .gitlab/scripts/create_release.py
+++ .gitlab/scripts/create_release.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 
 if __name__ == "__main__":
-    from changegen import CHANGELOG_FILE, ChangeGen
+    from commitgen import CommitGen
     from gitlab import Gitlab
     from versiontag import VersionTag
 
     gl = Gitlab(verbose=True)
-    changelog = ChangeGen(gl).generate()
-    if changelog:
+    payload = CommitGen(gl).generate()
+    if payload:
         pending_version = VersionTag(gl).pending
-        print(f"Updating changelog and tagging to {pending_version}:")
-        print(changelog["content"])
+        print(f"Updating jupyter cache and changelog and tagging to {pending_version}:")
         branch = "main"
-        gl.push_file(CHANGELOG_FILE, branch, **changelog)
-        response = gl.get_single_repository_branch(branch)
-        commit_id = response["commit"]["id"]
+        response = gl.commit(
+            branch,
+            f"Updating jupyter cache and changelog for {pending_version}",
+            payload,
+        )
+        commit_id = response["id"]
         gl.add_tag(pending_version, commit_id, message=f"DAML {pending_version}")
     else:
         print("No changes to commit and tag.")
