4d03cb6b5c407942b3f6eb0eef4d7b5501633538
Andrew Weng <andrew.weng@ariacoustics.com>
2024-05-24 19:24:16 +0000

    clean up mnist fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,20 +13,18 @@ def mnist():
         category: Literal["train", "test"] = "train",
         dtype: Optional[type] = None,
         add_channels: Literal["channels_first", "channels_last", "none"] = "none",
-        unit_normalize: bool = False
+        unit_normalize: bool = False,
     ) -> Tuple[np.ndarray, np.ndarray]:
         path = download_mnist()
         with np.load(path, allow_pickle=True) as fp:
             images, labels = fp["x_" + category][:size], fp["y_" + category][:size]
-        if dtype is not None:
+        if dtype:
             images = images.astype(dtype)
 
-        rescale_factor = 1.0/255 if unit_normalize else 1
-
         if add_channels == "channels_last":
-            images = images[..., np.newaxis] * rescale_factor
+            images = images[..., np.newaxis]
         elif add_channels == "channels_first":
-            images = images[:, np.newaxis] * rescale_factor
-        return images, labels
+            images = images[:, np.newaxis]
+        return images / 255 if unit_normalize else images, labels
 
     return _method
