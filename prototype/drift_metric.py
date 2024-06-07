f524eed626c792a2a244025deb1a8496eab722b2
Robert Jullens <shaun.jullens@ariacoustics.com>
2024-06-07 19:07:39 +0000

    KSDrift as MAITE Metric

--- prototype/drift_metric.py
+++ prototype/drift_metric.py
@@ -1,23 +1,25 @@
 import numpy as np
 import torch
 from torchmetrics import Metric
+from torchmetrics.utilities.data import dim_zero_cat
 
 from daml.metrics.drift import KSDrift
 
 
 class DriftMetric(Metric):
-    def __init__(self, x_ref):
+    def __init__(self, x_ref, **kwargs):
+        super().__init__(**kwargs)
         if isinstance(x_ref, torch.Tensor):
             x_ref = x_ref.detach().cpu().numpy()
         self.x_ref: np.ndarray = x_ref
         self._drift = KSDrift(x_ref)
-        self.add_state("images", default=[])
+        self.add_state("images", default=[], dist_reduce_fx="cat")
 
     def update(self, images: torch.Tensor):
         self.images.append(images)
 
-    def compute(self):
-        images: np.ndarray = self.images.detach().cpu().numpy()
+    def compute(self) -> dict:
+        images: np.ndarray = dim_zero_cat(self.images).detach().cpu().numpy()
         results = self._drift.predict(images)
 
         return results
