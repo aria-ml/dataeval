f676a7ed977d20a8090ca5ea5872cae872804bb6
Andrew Weng <andrew.weng@ariacoustics.com>
2024-06-13 03:45:35 +0000

    Add Linter and Duplicates detector classes

---
+++ tests/metrics/test_duplicates.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+from daml._internal.metrics.duplicates import Duplicates
+
+
+def get_dataset(count: int, channels: int):
+    return [np.random.random((channels, 16, 16)) for _ in range(count)]
+
+
+class TestDuplicates:
+    def test_duplicates(self):
+        data = np.random.random((20, 3, 16, 16))
+        dupes = Duplicates(np.concatenate((data, data)))
+        results = dupes.evaluate()
+        assert len(results["exact"]) == 20
+        assert len(results["near"]) == 0
+
+    def test_near_duplicates(self):
+        data = np.random.random((20, 3, 16, 16))
+        dupes = Duplicates(np.concatenate((data, data * 1.000001)))
+        results = dupes.evaluate()
+        assert len(results["exact"]) == 0
+        assert len(results["near"]) == 20
