c5abecf8531949c57803d6c9d0972d6b3f98b247
jbotts <jonathan.botts@ariacoustics.com>
2024-06-13 20:12:35 +0000

    add docstrings, remove obsolete functions

--- prototype/info_tools.py
+++ prototype/info_tools.py
@@ -21,48 +21,41 @@ def infer_categorical(X, threshold: float = 0.25):
     return pct_unique < threshold
 
 
-def compute_mi_class(props, class_var, cat_vars, con_vars, return_sorted=True):
+def compute_mutual_information(factors, is_categorical, num_neighbors=5):
     """
-    Mutual information (MI) with respect to a class label
+    Mutual information (MI) between factors (class label, metadata, label/image properties)
 
-    Notes:
-        - we use mutual_info_classif from sklearn since class label is categorical
-        - mutual_info_classif outputs are consistent up to O(1e-4) and depend on
-          a random seed
-        - MI is computed differently for categorical and continuous variables,
-          so we have to specify.
+    Parameters
+    ----------
+        factors: dict
+            Dict of arraylike variables including class labels, intrinsic
+            factors, and extrinsic factors
+        is_categorical: dict
+            Dictionary with factor names as keys and a boolean indicating
+            whether that variable is categorical.  True: categorical/discrete,
+            False: continuous
+        num_neighbors: int
+            Number of nearest neighbors to use for computing MI between discrete
+            and continuous variables.
 
-    Inputs:
-        - props: dict of arraylike variables including class labels, intrinsic
-          factors, and extrinsic factors
-        - class_var: str dictionary key corresponding to class label
-        - cat_vars: List[str] list of categorical or discrete variable names
-        - con_vars: List[str] list of continuous variable names
-        - return_sorted: bool if True, return sorted MI and corresponding
-          variable names
+    Notes
+    -----
+    - we use mutual_info_classif from sklearn since class label is categorical
+    - mutual_info_classif outputs are consistent up to O(1e-4) and depend on
+        a random seed
+    - MI is computed differently for categorical and continuous variables,
+        so we have to specify.
+
+    Returns
+    -------
+    np.ndarray, list
+
+    See Also
+    --------
+    sklearn.feature_selection.mutual_info_classif
+    sklearn.feature_selection.mutual_info_regression
+    sklearn.metrics.mutual_info_score
     """
-    tgt = props[class_var]
-    feat_cat = np.array([props[var] for var in cat_vars]).T
-    feat_con = np.array([props[var].astype(float) for var in con_vars]).T
-    all_feat = np.concatenate([feat_cat, feat_con], axis=1)
-    _vars = list(cat_vars) + list(con_vars)
-    cat_mask = [True for _ in range(len(cat_vars))] + [False for _ in range(len(con_vars))]
-    # units: nat
-    mi = mutual_info_classif(all_feat, tgt, discrete_features=cat_mask)
-
-    ent_class = entropy_(props[class_var], [True])
-    ent_all = entropy_(all_feat, cat_mask)
-    nmi = mi / np.sqrt(ent_class[0] * ent_all)
-
-    if return_sorted:
-        srt_inds = np.argsort(mi)
-        _vars = [_vars[i] for i in srt_inds]  # vars[srt_inds]
-        nmi = nmi[srt_inds]
-
-    return nmi, _vars
-
-
-def compute_mutual_information(factors, is_categorical, num_neighbors=5, return_sorted=False):
     # filter categorical/discrete and continuous variables
     _vars = list(factors.keys())
     cat_vars = [v for v in _vars if is_categorical[v]]
@@ -94,14 +87,47 @@ def compute_mutual_information(factors, is_categorical, num_neighbors=5, return_
     norm_factor = 0.5 * np.sum.outer(ent_all, ent_all) + 1e-6
     nmi = 0.5 * (mi + mi.T) / norm_factor
 
-    return nmi, var_names
+    return nmi, _vars
 
 
-def compute_mutual_information_class(factors, is_categorical, class_var="class", num_neighbors=5, return_sorted=False):
+def compute_mutual_information_class(factors, is_categorical, class_var="class", num_neighbors=5):
     """
-    Compute MI with respect to class for one class against the rest rather than
-    all classes together.  Look for correlation of one class with factors rather
-    than all classes with factors.
+    Compute mutual information of metadata factors (class label, metadata,
+    label/image properties) with individual classes.
+
+    Parameters
+    ----------
+        factors: dict
+            Dict of arraylike variables including class labels, intrinsic
+            factors, and extrinsic factors
+        is_categorical: dict
+            Dictionary with factor names as keys and a boolean indicating
+            whether that variable is categorical.  True: categorical/discrete,
+            False: continuous
+        class_var: str
+            name of the class label in the 'factors' dictionary
+        num_neighbors: int
+            Number of nearest neighbors to use for computing MI between discrete
+            and continuous variables.
+
+    Notes
+    -----
+    - we use mutual_info_classif from sklearn since class label is categorical
+    - mutual_info_classif outputs are consistent up to O(1e-4) and depend on
+        a random seed
+    - MI is computed differently for categorical and continuous variables,
+        so we have to specify.
+
+    Returns
+    -------
+    np.ndarray, list
+
+    See Also
+    --------
+    sklearn.feature_selection.mutual_info_classif
+    sklearn.feature_selection.mutual_info_regression
+    sklearn.metrics.mutual_info_score
+    compute_mutual_information
     """
 
     # filter categorical/discrete and continuous variables
@@ -131,38 +157,6 @@ def compute_mutual_information_class(factors, is_categorical, class_var="class",
     ent_tgt = entropy_(factors[class_var], [True])
     norm_factor = 0.5 * np.sum.outer(ent_tgt, ent_all) + 1e-6
     nmi = mi / norm_factor
-
-    return nmi, var_names
-
-
-def compute_corr_matrix(props, cat_vars, con_vars, num_neighbors=7):
-    num_cat, num_con = len(cat_vars), len(con_vars)
-    num_feat = num_cat + num_con
-    mi = np.empty((num_feat, num_feat))
-    mi[:] = np.nan
-    feat_cat = np.array([props[var] for var in cat_vars]).T
-    feat_con = np.array([props[var].astype(float) for var in con_vars]).T
-    all_feat = np.concatenate([feat_cat, feat_con], axis=1)
-    cat_mask = [True for _ in range(len(cat_vars))] + [False for _ in range(len(con_vars))]
-    _vars = list(cat_vars) + list(con_vars)
-    # classification MI for discrete/categorical features
-    for idx, tgt_var in enumerate(cat_vars):
-        tgt = props[tgt_var]
-        # units: nat
-        mi[idx, :] = mutual_info_classif(all_feat, tgt, discrete_features=cat_mask, n_neighbors=num_neighbors)
-
-    # regression MI for continuous features
-    for idx, tgt_var in enumerate(con_vars):
-        tgt = props[tgt_var]
-        # units: nat
-        mi[idx + num_cat, :] = mutual_info_regression(
-            all_feat, tgt, discrete_features=cat_mask, n_neighbors=num_neighbors
-        )
-
-    ent_all = entropy_(all_feat, cat_mask)
-    norm_factor = 0.5 * np.sum.outer(ent_all, ent_all) + 1e-6
-    nmi = 0.5 * (mi + mi.T) / norm_factor
-
     return nmi, _vars
 
 
