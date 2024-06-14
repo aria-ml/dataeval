d07e6fc4ecebc5870f277037fd3f73a06f04a5bf
Robert Jullens <shaun.jullens@ariacoustics.com>
2024-06-14 20:17:20 +0000

    Keeping aria copy up-to-date with Increment-5 version

--- prototype/demoprep/daml_stage.py
+++ prototype/demoprep/daml_stage.py
@@ -1,18 +1,15 @@
 import json
 from collections import defaultdict
 from pathlib import Path
-from typing import List, Literal, Optional, TypeAlias
+from typing import Callable, Dict, List, Literal, Optional, TypeAlias
 
 from test_stage import TestStage
 
-Dataset_T: TypeAlias = List[Literal["development", "operational", "both"]]
+Dataset_T: TypeAlias = List[Literal["development", "operational"]]
 Checkbox: TypeAlias = Optional[List[str]]
 
 
 class DamlTestStage(TestStage):
-    DEV_OPTS = ["development", "both"]
-    OPR_OPTS = ["operational", "both"]
-    BOTH = ["development", "operational"]
     _cache_dir = Path(".daml_cache")
     _cache_file = Path("cache.json")
 
@@ -22,11 +19,11 @@ class DamlTestStage(TestStage):
         linting_options: Checkbox = None,
         linting_dataset: Dataset_T = ["development", "operational"],
         bias_options: Checkbox = ["balance", "coverage", "parity"],
-        bias_dataset: Dataset_T = ["development"],
+        bias_dataset: Dataset_T = ["development", "operational"],
         drift: bool = False,
         ood_detection: bool = False,
-        feasibility_dataset: Dataset_T = ["development"],
-        sufficiency_dataset: Dataset_T = ["development"],
+        feasibility_dataset: Dataset_T = ["development", "operational"],
+        sufficiency_dataset: Dataset_T = ["development", "operational"],
         *args,
         **kwargs,
     ):
@@ -50,30 +47,31 @@ class DamlTestStage(TestStage):
         self.cached_images = {}  # {dataset: embeddings}
         self.cached_labels = {}  # {dataset: embeddings}
 
-        self.load_cached_results(self._cache_dir / self._cache_file)
-
     def run(self) -> None:
         """Run the test stage, and store any outputs of the evaluation in test stage"""
 
+        self.load_cached_results(self._cache_dir / self._cache_file)
         if self.cache:
             self.outputs = self.cache
-            return
+            # return
+
+        def update_dict(d1: dict, d2: dict):
+            """Updates d1 with d2s content"""
+            for k, v in d2.items():
+                d1[k].update(v)
+            return d1
 
         metric_outputs = defaultdict(dict)
 
-        # Run metrics that take individual datasets
-        for k, v in self.feasibility().items():
-            metric_outputs[k].update(v)
-        for k, v in self.bias.items():
-            metric_outputs[k].update(v)
-        for k, v in self.sufficiency().items():
-            metric_outputs[k].update(v)
-
-        # metric_outputs.update(self.drift_detection())
+        metric_outputs = update_dict(metric_outputs, self.feasibility())
+        metric_outputs = update_dict(metric_outputs, self.bias())
+        metric_outputs = update_dict(metric_outputs, self.sufficiency())
+        metric_outputs = update_dict(metric_outputs, self.drift_detection())
+        metric_outputs = update_dict(metric_outputs, self.outlier_detection())
 
         self.outputs = metric_outputs
 
-        # self._save_cache()
+        self._save_cache()
 
     def load_cached_results(self, results: Path) -> None:
         """Load cached results from a previous run so that they may be accessed with the collect_metrics and the
@@ -88,12 +86,6 @@ class DamlTestStage(TestStage):
             self.cache = {}
 
     def _save_cache(self) -> None:
-        # Update cache
-        for dataset, metric in self.outputs.items():
-            if dataset not in self.cache:
-                self.cache[dataset] = {}
-            self.cache[dataset].update(metric)
-
         # Check path
         folder = self._cache_dir
         folder.mkdir(exist_ok=True)
@@ -101,7 +93,7 @@ class DamlTestStage(TestStage):
 
         # Save results
         with cache_file_path.open("w+") as f:
-            json.dump(self.cache, f, indent=4, sort_keys=True)
+            json.dump(self.outputs, f, indent=4, sort_keys=True)
 
     def collect_metrics(self):
         print("Returning metrics")
@@ -111,36 +103,35 @@ class DamlTestStage(TestStage):
         print("Returning Gradient parameters")
         return self.outputs
 
-    def get_dataset(self, select: str):
-        if select == "development":
-            return {"development": self.dev_dataset}
-        elif select == "operational":
-            return {"operational": self.operational_dataset}
-        else:
-            return {"development": self.dev_dataset, "operational": self.operational_dataset}
+    def get_dataset(self, select: Dataset_T) -> dict:
+        ds = {"development": self.dev_dataset, "operational": self.operational_dataset}
+        return {name: ds[name] for name in select}
 
-    def run_metric(self, select, metric_dict, opts):
+    def run_metric(self, select: Dataset_T, metric_dict: Dict[str, Callable], opts: List[str]):
+        """
+        Sets the dataset name as outer key, metric as inner key, dict output as value
+
+        ex. {"development": "ber": {"ber": 0.18, "ber_lower"}}
+        """
         output = defaultdict(dict)
 
-        for dataset in self.get_dataset(select=select):
-            results = {}
-            for name in opts:
-                result = metric_dict[name](dataset)
-                results.update(result)
-            output[dataset].update(results)
+        for name, dataset in self.get_dataset(select).items():
+            output[name].update({metric: metric_dict[metric](dataset) for metric in opts})
 
+        return output
+
+    def run_comparison_metric(self, metric_dict):
+        """Run metrics that take in two datasets"""
+        output = {name: metric(self.dev_dataset, self.operational_dataset) for name, metric in metric_dict.items()}
         return output
 
     def feasibility(self):
         METRICS_DICT = {"ber": self._ber}
-        opts = ["ber"]
+        opts: Checkbox = ["ber"]
 
         feasibility_output = self.run_metric(self.feasibility_dataset, METRICS_DICT, opts)
 
         return feasibility_output
-
-    def _ber(self, dataset) -> dict:
-        return {"ber": {"ber": 0.18, "ber_lower": 0.095}}
 
     def bias(self) -> dict:
         METRICS_DICT = {"balance": self._balance, "coverage": self._coverage, "parity": self._parity}
@@ -150,25 +141,42 @@ class DamlTestStage(TestStage):
 
         return bias_output
 
-    def _coverage(self, dataset) -> dict:
-        return {"coverage": {"value": 0.90}}
-
-    def _parity(self, dataset) -> dict:
-        return {"parity": {"value": 0.25}}
-
-    def _balance(self, dataset) -> dict:
-        return {"balance": {"value": 0.5}}
-
-    def linting(self) -> dict:
-        return {}
-
     def sufficiency(self) -> dict:
         METRICS_DICT = {"sufficiency": self._sufficiency}
-        opts = ["sufficiency"]
+        opts: Checkbox = ["sufficiency"]
 
         sufficiency_output = self.run_metric(self.sufficiency_dataset, METRICS_DICT, opts)
 
         return sufficiency_output
+
+    def linting(self) -> dict:
+        return {}
+
+    def outlier_detection(self) -> dict:
+        # outlier_detector = AEOutlier(create_model(AE, (28, 28, 1)))
+        # TODO: Need to swap PyTorch NCHW to TensorFlow NHWC
+        # self.outlier_detector.fit(train_dataset)
+        # output = self.outlier_detector.predict(val_dataset)
+        # return output
+        return {}
+
+    def drift_detection(self) -> dict:
+        METRICS_DICT = {"mmd": self._mmd, "ks": self._ks}
+        drift_output = self.run_comparison_metric(METRICS_DICT)
+
+        return drift_output
+
+    def _ber(self, dataset) -> dict:
+        return {"ber": 0.18, "ber_lower": 0.095}
+
+    def _coverage(self, dataset) -> dict:
+        return {"value": 0.90}
+
+    def _parity(self, dataset) -> dict:
+        return {"value": 0.25}
+
+    def _balance(self, dataset) -> dict:
+        return {"factors": [], "classes": []}
 
     def _sufficiency(self, dataset) -> dict:
         """
@@ -190,17 +198,12 @@ class DamlTestStage(TestStage):
 
         """
 
-        output = {"sufficiency": {"model": [1, 2, 3], "comp_model": [2, 3, 4], "steps": [1, 2, 3]}}
+        output = {"model": [1, 2, 3], "comp_model": [2, 3, 4], "steps": [1, 2, 3]}
 
         return output
 
-    def outlier_detection(self) -> dict:
-        # outlier_detector = AEOutlier(create_model(AE, (28, 28, 1)))
-        # TODO: Need to swap PyTorch NCHW to TensorFlow NHWC
-        # self.outlier_detector.fit(train_dataset)
-        # output = self.outlier_detector.predict(val_dataset)
-        # return output
-        return {}
+    def _mmd(self, dataset1, dataset2):
+        return {"pvalue": 1.0, "is_drift": True, "val": 0.7}
 
-    def drift_detection(self) -> dict:
-        return {}
+    def _ks(self, dataset1, dataset2):
+        return {"pvalue": 0.8, "is_drift": False, "val": 0.1}
