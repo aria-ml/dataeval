18063d43070b821ebff12fd4426e73bdce43dd8c
Andrew Weng <andrew.weng@ariacoustics.com>
2024-07-17 12:30:31 -0700

    Revert "Use separate Harbor repositories for each branch"
    This reverts commit 0be70de83ea98d40050239da200f333476509213.

--- .gitlab/scripts/remove_image_tags.py
+++ .gitlab/scripts/remove_image_tags.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 if __name__ == "__main__":
     import re
 
@@ -11,15 +12,18 @@ if __name__ == "__main__":
 
     merged_set = {mr["source_branch"] for mr in gl.list_merge_requests("merged", "main")}
     opened_set = {mr["source_branch"] for mr in gl.list_merge_requests("opened", "main")}
-    delete_set = {re.sub(r"[^a-zA-Z0-9]+", "-", branch_name) for branch_name in (merged_set - opened_set)}
+    delete_set = merged_set - opened_set
 
     # Build list of registry image tags that should be deleted
-    repositories = []
-    for branch_name in delete_set:
-        for repository in hb.list_repositories(branch_name):
-            repositories.append(repository["name"])
+    registry_tags = {"cache": [], "dev": []}
+    for repository in registry_tags:
+        for artifact in hb.list_artifacts(repository):
+            for tag in artifact["tags"]:
+                if any(re.sub(r"[^a-zA-Z0-9]+", "-", branch_name) in tag["name"] for branch_name in delete_set):
+                    registry_tags[repository].append(tag["name"])
 
     # Delete the collected registry image tags
-    for repository in repositories:
-        print(f"Removing {repository}...")
-        hb.delete_repository(repository)
+    for repository, tags in registry_tags.items():
+        for tag in tags:
+            print(f"Removing {tag} from {repository}...")
+            hb.delete_tag(repository, tag)
