79f84037f20f41d3bf1c2dfa073ddecb7199c228
Andrew Weng <andrew.weng@ariacoustics.com>
2024-07-17 19:37:22 +0000

    Fix tag cleanup for artifacts without tags

--- .gitlab/scripts/remove_image_tags.py
+++ .gitlab/scripts/remove_image_tags.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 if __name__ == "__main__":
     import re
 
@@ -12,14 +11,14 @@ if __name__ == "__main__":
 
     merged_set = {mr["source_branch"] for mr in gl.list_merge_requests("merged", "main")}
     opened_set = {mr["source_branch"] for mr in gl.list_merge_requests("opened", "main")}
-    delete_set = merged_set - opened_set
+    delete_set = {re.sub(r"[^a-zA-Z0-9]+", "-", branch) for branch in (merged_set - opened_set)}
 
     # Build list of registry image tags that should be deleted
     registry_tags = {"cache": [], "dev": []}
     for repository in registry_tags:
         for artifact in hb.list_artifacts(repository):
-            for tag in artifact["tags"]:
-                if any(re.sub(r"[^a-zA-Z0-9]+", "-", branch_name) in tag["name"] for branch_name in delete_set):
+            for tag in artifact["tags"] or []:
+                if any(branch in tag["name"] for branch in delete_set):
                     registry_tags[repository].append(tag["name"])
 
     # Delete the collected registry image tags
