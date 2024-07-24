be8b70634cc95d22589d1537a14e2af53ee54c94
Andrew Weng <andrew.weng@ariacoustics.com>
2024-07-24 10:49:26 -0700

    Separate data download logic from docs config

--- docs/conf.py
+++ docs/conf.py
@@ -3,8 +3,7 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-from contextlib import contextmanager
-from os import chdir, getcwd, getenv
+from os import getenv
 
 # -- Project information ---------------------------------------------------
 project = "DataEval"
@@ -81,39 +80,6 @@ html_static_path = ["_static"]
 doctest_test_doctest_blocks = ""
 
 
-@contextmanager
-def cwd(path):
-    old_path = getcwd()
-    chdir(path)
-    try:
-        yield
-    finally:
-        chdir(old_path)
-
-
-def predownload_data():
-    import keras.datasets as kds
-    import tensorflow_datasets as tfds
-    from torchvision.datasets import MNIST
-
-    # Assume we are running in the docs directory with notebooks in tutorials/notebooks
-    with cwd("./tutorials/notebooks"):
-        # AETrainerTutorial.ipynb
-        # ClassLearningCurvesTutorial.ipynb
-        # ClassLabelAnalysisTutorial.ipynb
-        MNIST(root="./data/", train=True, download=True)
-        MNIST(root="./data/", train=False, download=True)
-
-        # BayesErrorRateEstimationTutorial.ipynb
-        kds.mnist.load_data()
-
-        # DriftDetectionTutorial.ipynb
-        # HPDivergenceTutorial.ipynb
-        # OODDetectionTutorial.ipynb
-        tfds.load("mnist", split="train")
-        tfds.load("mnist_corrupted/translate", split="train")
-
-
 # because we expose private modules in public namespaces
 # and rename some classes, documentation recognizes these
 # public classes as aliases, which we don't want
@@ -129,7 +95,13 @@ def normalize_module(mod_names):
 
 
 def setup(app):
-    predownload_data()
+    # pre-download data used in notebooks
+    from os import getcwd
+    from sys import path
+    path.append(getcwd())
+    import data
+    data.download()
+
     normalize_module(
         [
             "dataeval.detectors",
