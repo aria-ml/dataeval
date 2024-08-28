b0daacaaef12ea2167ffe8774444e5be4a39155b
Robert Jullens <shaun.jullens@ariacoustics.com>
2024-08-28 22:35:36 +0000

    Refactored read_dataset to handle common dataset returns

--- src/dataeval/_internal/utils.py
+++ src/dataeval/_internal/utils.py
@@ -1,67 +1,64 @@
-from typing import List, Tuple
+from collections import defaultdict
+from typing import Any, Dict, List
 
-from numpy.typing import ArrayLike
 from torch.utils.data import Dataset
 
-from dataeval._internal.interop import is_arraylike
 
-
-def _validate_getitem(dataset: Dataset, min_length: int):
+def read_dataset(dataset: Dataset) -> List[List[Any]]:
     """
-    Checks if return value is type tuple and has at least min_length ArrayLike values to parse
-
-    Parameters
-    ----------
-    dataset : Dataset
-        Collection of data
-    min_length : int
-        Minimum number of entries within data that are ArrayLike
-    """
-
-    if min_length < 1:
-        raise ValueError(f"Minimum tuple length cannot be less than 1, got {min_length}")
-
-    data = next(iter(dataset))
-    if not isinstance(data, tuple):
-        raise TypeError(f"Expected return type of tuple, got {type(data)}")
-
-    if len(data) < min_length:
-        raise ValueError(f"Expected length of {min_length} or more in tuple, got {len(data)}")
-
-    for i in range(min_length):
-        if not is_arraylike(data[i]):
-            raise TypeError(f"Expected ArrayLike in return position {i}, got {type(data[i])}")
-
-
-def read_dataset(dataset: Dataset) -> Tuple[List[ArrayLike], List[ArrayLike]]:
-    """
-    Extract images and labels from a dataset
+    Extract information from a dataset at each index into a individual lists of each information position
 
     Parameters
     ----------
     dataset : torch.utils.data.Dataset
-        Dataset that returns (images, labels).
+        Input dataset
 
     Returns
     -------
-    Tuple[List[ArrayLike], [List[ArrayLike]]]
-        Separate images and label lists in native backend
+    List[List[Any]]
+        All objects in individual lists based on return position from dataset
 
-    Note
-    ----
-    This function ignores additional return values in __getitem__ such as metadata.
+    Warning
+    -------
+    No type checking is done between lists or data inside lists
 
-    This function returns a list in case images have inhomogeneous shapes.
+    See Also
+    --------
+    torch.utils.data.Dataset
 
+    Examples
+    --------
+    >>> import numpy as np
+
+    >>> data = np.ones((10, 3, 3))
+    >>> labels = np.ones((10,))
+    >>> class ICDataset:
+    ...     def __init__(self, data, labels):
+    ...         self.data = data
+    ...         self.labels = labels
+
+    ...     def __getitem__(self, idx):
+    ...         return self.data[idx], self.labels[idx]
+
+    >>> ds = ICDataset(data, labels)
+
+    >>> result = read_dataset(ds)
+    >>> assert len(result) == 2
+    True
+    >>> assert result[0].shape == (10, 3, 3)  # 10 3x3 images
+    True
+    >>> assert result[1].shape == (10,)  # 10 labels
+    True
     """
-    images = []
-    labels = []
 
-    _validate_getitem(dataset=dataset, min_length=2)  # Images, labels
+    ddict: Dict[int, List] = defaultdict(list)
 
-    # Only want first two values, with the assumption (image, label, ...)
     for data in dataset:
-        images.append(data[0])
-        labels.append(data[1])
+        # Convert to tuple if single return (e.g. images only)
+        if not isinstance(data, tuple):
+            data = (data,)
 
-    return images, labels
+        for i, d in enumerate(data):
+            ddict[i].append(d)
+
+    return list(ddict.values())
