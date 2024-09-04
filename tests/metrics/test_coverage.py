1a004a583fb64317e47538a8459330590d32892e
Robert Jullens <shaun.jullens@ariacoustics.com>
2024-09-04 23:33:24 +0000

    Unify handling of image reshaping

--- tests/metrics/test_coverage.py
+++ tests/metrics/test_coverage.py
@@ -11,22 +11,34 @@ class TestCoverageUnit:
     def test_fails_with_invalid_radius_type(self):
         embs = np.zeros((100, 2))
         with pytest.raises(ValueError), warnings.catch_warnings():
-            coverage(embs, radius_type="new")  # type: ignore
+            coverage(embs, radius_type="NOT_A_RADIUS_TYPE")  # type: ignore
 
     def test_n_too_small(self):
         embs = np.zeros((3, 2))
         with pytest.raises(ValueError), warnings.catch_warnings():
-            coverage(embs, radius_type="naive")
+            coverage(embs, k=3)
 
     def test_naive(self):
+        """Checks pvals, crit, rho are all acceptable values"""
         embs = np.zeros((3, 2))
         result = coverage(embs, "naive", k=1)
         assert abs(result.critical_value - math.sqrt(2 / 3) / math.sqrt(math.pi)) < 0.01
 
     def test_adaptive(self):
+        """Checks pvals, crit, rho are all acceptable values"""
         embs = np.zeros((100, 2))
         result = coverage(embs, "adaptive", k=1)
-        assert (result.radii == np.zeros(100)).all()
+        np.testing.assert_array_equal(result.radii, np.zeros(100))
+
+    def test_high_dim_data(self):
+        """High dimensional data should not affect calculations"""
+        embs = np.random.random(size=(100, 16, 16))
+        x = coverage(embs)
+        x_flat = coverage(embs.reshape((100, -1)))
+
+        assert x.critical_value == x_flat.critical_value
+        np.testing.assert_array_equal(x.indices, x_flat.indices)
+        np.testing.assert_array_equal(x.radii, x_flat.radii)
 
 
 class TestCoverageFunctional:
