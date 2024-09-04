9f4df773fb1a35b2187d1071199280540b035afc
jbotts <jonathan.botts@ariacoustics.com>
2024-09-04 14:06:24 -0400

    add test for entropy

--- tests/metrics/test_diversity.py
+++ tests/metrics/test_diversity.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytest
 
+from dataeval._internal.metrics.utils import entropy
 from dataeval.metrics import diversity, diversity_classwise
 
 num_samples = 20
@@ -9,6 +10,23 @@ metadata = [
     {"var_cat": vals[np.random.randint(low=0, high=len(vals))], "var_cnt": np.random.randn()} for _ in range(20)
 ]
 class_labels = [np.random.randint(low=0, high=2) for _ in range(num_samples)]
+
+
+@pytest.fixture
+def entropy_test_vars():
+    ent = {}
+    ent["is_categorical"] = [True, False]
+    ent["data"] = np.stack((np.ones(10, dtype=int), np.random.randint(low=0, high=8, size=10))).T
+    ent["names"] = ["a", "b"]
+    return ent
+
+
+@pytest.mark.parametrize("norm", [True, False])
+def test_entropy_normalization(norm, entropy_test_vars):
+    ent = entropy(
+        entropy_test_vars["data"], entropy_test_vars["names"], entropy_test_vars["is_categorical"], normalized=norm
+    )
+    assert ent[0] == 0
 
 
 @pytest.mark.parametrize("div_fn", [diversity, diversity_classwise])
