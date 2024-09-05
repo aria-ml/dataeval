c3dd71860483f73317abb049350829d2a2eca126
Max Bright <max.bright@ariacoustics.com>
2024-09-05 14:16:22 -0700

    Moved model calls out of loss estimator

--- prototype/loss_estimation.py
+++ prototype/loss_estimation.py
@@ -15,6 +15,54 @@ from torch.utils.data import DataLoader, Dataset, Subset
 import nannyml as nml
 from IPython.display import display
 
+def outputs_to_nannyml(problem_type, outputs, class_names, truths=[]):
+    classification = ("classification" in problem_type)
+    has_labels = len(truths) > 0
+    pred_type = int if classification else float
+    dict_out = {"y_pred": np.zeros(0, dtype=pred_type)}
+    if has_labels:
+        dict_out["y"] = np.zeros(0, dtype=pred_type)
+
+    if classification:
+        for class_name in class_names:
+            dict_out[f"y_pred_proba_{class_name}"] = np.zeros(0)
+    
+
+
+    # Set model layers into evaluation mode
+    #model.eval()
+    #dataloader = DataLoader(dataset, batch_size=16)
+    # Tell PyTorch to not track gradients, greatly speeds up processing
+    for batch_id, output_batch in enumerate(outputs):
+            #batch 
+            # Load data/images to device
+            #X = torch.Tensor(batch[0]).to(device)
+            # Load targets/labels to device
+            
+            #output = model(X).cpu()
+            
+            
+
+            if classification:
+                preds = np.int64(torch.argmax(output_batch, dim=1))
+                for i, class_name in enumerate(class_names):
+                    key = f"y_pred_proba_{class_name}"
+                    dict_out[key] = np.concatenate((dict_out[key], output_batch[:, i]))
+            else:
+                preds = output_batch[:,0]
+            dict_out["y_pred"] = np.concatenate((dict_out["y_pred"], preds), dtype=pred_type)
+
+            if has_labels:
+                y_batch = truths[batch_id]
+                if classification:
+                    y = torch.Tensor(y_batch).int()
+                else:
+                    y = torch.Tensor(y_batch).float()
+                dict_out["y"] = np.concatenate((dict_out["y"], y), dtype=pred_type)
+            
+    return pd.DataFrame(dict_out)
+
+
 class LossEstimator():
     def __init__(self, method="CBPE", problem_type="classification_multiclass"):
         self.method = method
@@ -65,7 +113,48 @@ class LossEstimator():
 
         return dict_out
 
-    def evaluate(self, model, ref_data, op_data, class_names, chunk_size=50, eval_kwargs={}):
+    def evaluate(self, ref_df, op_df, class_names, chunk_size=50):
+        if self.classification:
+            y_pred_keys = {}
+            for class_name in class_names:
+                y_pred_keys[class_name] = f"y_pred_proba_{class_name}"
+        else:
+            feature_keys = []#set(class_names)
+
+        est_kwargs = {
+            #'problem_type': self.problem_type,
+            #'y_pred_proba': y_pred_keys,
+            'y_pred': 'y_pred',
+            'y_true': 'y',
+            'metrics': [self.metric],
+            'chunk_size': chunk_size
+        }
+
+        if self.classification:
+            estimator = nml.CBPE(problem_type=self.problem_type,
+                                y_pred_proba = y_pred_keys,
+                                **est_kwargs)
+        else:
+            estimator = nml.DLE(feature_column_names = feature_keys,
+                **est_kwargs)
+        
+
+        estimator.fit(ref_df)
+        results = estimator.estimate(op_df)
+
+        reference_df = results.filter(period="reference").to_df()
+        results_df = results.filter(period="analysis").to_df()
+        ref_accuracy = np.mean(reference_df[self.metric]['value'])
+        pred_accuracy = np.mean(results_df[self.metric]['value'])
+        alert = np.any(results_df[self.metric]['alert'])
+
+        return {
+            "Reference_Metric": ref_accuracy,
+            "Op_Predicted_Metric": pred_accuracy,
+            "Has_Drifted": alert
+        }
+
+    def old_evaluate(self, model, ref_data, op_data, class_names, chunk_size=50, eval_kwargs={}):
 
         ref_dict = self._eval_model(model, ref_data, class_names, True, **eval_kwargs)
         ref_df = pd.DataFrame(ref_dict)
