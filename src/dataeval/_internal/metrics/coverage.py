545c4288bc4f4a54f0804537cbbf547a7323b7ca
Ryan Wood <ryan.wood@ariacoustics.com>
2024-09-09 16:57:09 +0000

    Adding clustering/coverage tutorial

--- src/dataeval/_internal/metrics/coverage.py
+++ src/dataeval/_internal/metrics/coverage.py
@@ -66,27 +66,22 @@ def coverage(
 
     Note
     ----
-    Embeddings should be on the unit interval.
+    Embeddings should be on the unit interval [0-1].
 
     Example
     -------
-    >>> coverage(embeddings)
-    CoverageOutput(indices=array([], dtype=int64), radii=array([0.59307666, 0.56956307, 0.56328616, 0.70660265, 0.57778087,
-           0.53738624, 0.58968217, 1.27721334, 0.84378694, 0.67767021,
-           0.69680335, 1.35532621, 0.59764166, 0.8691945 , 0.83627602,
-           0.84187303, 0.62212358, 1.09039732, 0.67956797, 0.60134383,
-           0.83713908, 0.91784263, 1.12901193, 0.73907618, 0.63943983,
-           0.61188447, 0.47872713, 0.57207771, 0.92885883, 0.54750511,
-           0.83015726, 1.20721778, 0.50421928, 0.98312246, 0.59764166,
-           0.61009202, 0.73864073, 1.0381061 , 0.77598609, 0.72984036,
-           0.67573006, 0.48056064, 1.00050879, 0.89532971, 0.58395529,
-           0.95954793, 0.60134383, 1.10096454, 0.51955314, 0.73038702]), critical_value=0)
+    >>> results = coverage(embeddings)
+    >>> results.indices
+    array([447, 412,   8,  32,  63])
+    >>> results.critical_value
+    0.8459038956941765
 
     Reference
     ---------
     This implementation is based on https://dl.acm.org/doi/abs/10.1145/3448016.3457315.
+
     [1] Seymour Sudman. 1976. Applied sampling. Academic Press New York (1976).
-    """  # noqa: E501
+    """
 
     # Calculate distance matrix, look at the (k+1)th farthest neighbor for each image.
     embeddings = to_numpy(embeddings)
@@ -105,8 +100,9 @@ def coverage(
         pvals = np.where(crit > rho)[0]
     elif radius_type == "adaptive":
         # Use data adaptive cutoff as rho
-        rho = int(n * percent)
-        pvals = np.argsort(crit)[::-1][:rho]
+        selection = int(max(n * percent, 1))
+        pvals = np.argsort(crit)[::-1][:selection]
+        rho = float(np.mean(np.sort(crit)[::-1][selection - 1 : selection + 1]))
     else:
         raise ValueError(f"{radius_type} is an invalid radius type. Expected 'adaptive' or 'naive'")
     return CoverageOutput(pvals, crit, rho)
