f4b9f2c91d4afaf392b304e839fcfe2ccc90b52d
Andrew Weng <andrew.weng@ariacoustics.com>
2024-09-18 16:49:23 +0000

    Tag LKG at the correct commit SHA

--- .gitlab/scripts/update_latest_known_good.py
+++ .gitlab/scripts/update_latest_known_good.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
 if __name__ == "__main__":
+    from os import environ
+
     from gitlab import LATEST_KNOWN_GOOD, Gitlab
+
+    ref = environ.get("CI_COMMIT_SHA") or "main"
 
     gl = Gitlab(verbose=True)
     gl.delete_tag(LATEST_KNOWN_GOOD)
-    gl.add_tag(LATEST_KNOWN_GOOD)
+    gl.add_tag(LATEST_KNOWN_GOOD, ref)
