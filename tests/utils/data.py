718275bd0db875276d91fdfbd126b6a2f911715b
Justin McMillan <justin.mcmillan@ariacoustics.com>
2024-09-19 21:17:43 +0000

    Add dataset splitting features

--- tests/utils/data.py
+++ tests/utils/data.py
@@ -66,3 +66,80 @@ class DataEvalDataset(Dataset):
                 assert len(self._boxes) == len(self._labels)
         else:
             assert self._boxes is None
+
+
+class DataEvalWithMetadataDataset(Dataset):
+    """Holds the arrays of images, labels, bounding boxes, and metadata"""
+
+    def __init__(
+        self,
+        images: Any,
+        labels: Any = None,
+        boxes: Any = None,
+        metadata: Any = None,
+    ) -> None:
+        self._images: Any = images
+        self._labels = labels
+        self._boxes = boxes
+        self._metadata = metadata
+        self._validate()
+
+    def __len__(self) -> int:
+        return len(self._images)
+
+    def __getitem__(self, index) -> Any | tuple:
+        image: Any = self._images[index]
+        # Return image if no other attributes
+        if self._labels is None:
+            output: Any = [
+                image,
+            ]
+        labels: Any = self._labels[index]
+        # Return image and label for image classification
+        if self._boxes is None:
+            output: Any = [image, labels]
+        # Return images, labels, boxes for object detection
+        output: Any = [image, labels, self._boxes[index]]
+        if self._metadata is not None:
+            output.append(self._metadata[index])
+        else:
+            output.append({})
+        return tuple(output)
+
+    @property
+    def images(self) -> Any:
+        return self._images
+
+    @property
+    def labels(self) -> Any:
+        if self._labels is None:
+            return np.array([])
+        else:
+            return self._labels
+
+    @property
+    def boxes(self) -> Any:
+        if self._boxes is None:
+            return np.array([])
+        else:
+            return self._boxes
+
+    @property
+    def metadata(self) -> Any:
+        if self._metadata is None:
+            return np.array([])
+        else:
+            return self._metadata
+
+    def _validate(self):
+        if self._labels is not None:
+            if self._boxes is not None:
+                assert len(self._boxes) == len(self._labels)
+            else:
+                assert len(self._images) == len(self._labels)
+            if self._metadata is not None:
+                assert len(self._metadata) == len(self._labels)
+        else:
+            if self._metadata is not None:
+                assert len(self._metadata) == len(self._images)
+            assert self._boxes is None
