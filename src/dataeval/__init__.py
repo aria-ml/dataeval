068df49a213b3799c31eb492f583c822148cbadb
Andrew Weng <andrew.weng@ariacoustics.com>
2024-10-18 19:52:07 -0700

    wip

--- src/dataeval/__init__.py
+++ src/dataeval/__init__.py
@@ -6,17 +6,38 @@ _IS_TORCH_AVAILABLE = find_spec("torch") is not None
 _IS_TENSORFLOW_AVAILABLE = find_spec("tensorflow") is not None and find_spec("tensorflow_probability") is not None
 
 del find_spec
+import apipkg
 
-from . import detectors, metrics  # noqa: E402
+apipkg.initpkg(
+    __name__,
+    {
+        "detectors": {
+            "drift": {
+                "DriftCVM": "dataeval._internal.detectors.drift:DriftCVM",
+                "DriftKS": "dataeval._internal.detectors.drift:DriftKS",
+                "DriftOutput": "dataeval._internal.detectors.drift:DriftOutput",
+            },
+            "linters": {
+                "Clusterer": "dataeval._internal.detectors.clusterer:Clusterer",
+                "ClustererOutput": "dataeval._internal.detectors.clusterer:Clusterer",
+                "Duplicates": "dataeval._internal.detectors.duplicates:Duplicates",
+                "DuplicatesOutput": "dataeval._internal.detectors.duplicates:DuplicatesOutput",
+                "Outliers": "dataeval._internal.detectors.outliers:Outliers",
+                "OutliersOutput": "dataeval._internal.detectors.outliers:OutliersOutput",
+            },
+        }
+    },
+)
+# from . import detectors, metrics
 
-__all__ = ["detectors", "metrics"]
+# __all__ = ["detectors", "metrics"]
 
-if _IS_TORCH_AVAILABLE:  # pragma: no cover
-    from . import workflows
+# if _IS_TORCH_AVAILABLE:  # pragma: no cover
+#     from . import workflows
 
-    __all__ += ["workflows"]
+#     __all__ += ["workflows"]
 
-if _IS_TENSORFLOW_AVAILABLE or _IS_TORCH_AVAILABLE:  # pragma: no cover
-    from . import utils
+# if _IS_TENSORFLOW_AVAILABLE or _IS_TORCH_AVAILABLE:  # pragma: no cover
+#     from . import utils
 
-    __all__ += ["utils"]
+#     # __all__ += ["utils"]
--- src/dataeval/detectors/__init__.py
+++ src/dataeval/detectors/__init__.py
@@ -9,6 +9,19 @@ from . import drift, linters
 __all__ = ["drift", "linters"]
 
 if _IS_TENSORFLOW_AVAILABLE:  # pragma: no cover
-    from . import ood
+    # from . import ood
+    # __all__ += ["ood"]
+    import apipkg
 
-    __all__ += ["ood"]
+    apipkg.initpkg(
+        __name__,
+        {
+            "ood": {
+                "OOD_AE": "dataeval._internal.detectors.ood.ae:OOD_AE",
+                "OOD_AEGMM": "dataeval._internal.detectors.ood.aegmm:OOD_AEGMM",
+                "OOD_LLR": "dataeval._internal.detectors.ood.llr:OOD_LLR",
+                "OOD_VAE": "dataeval._internal.detectors.ood.vae:OOD_VAE",
+                "OOD_VAEGMM": "dataeval._internal.detectors.ood.vae:OOD_VAEGMM",
+            }
+        },
+    )
