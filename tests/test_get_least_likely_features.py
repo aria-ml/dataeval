0db3efeea78bb1a947911a54c93ff9c9182b0201
Bill Peria <bill.peria@ariacoustics.com>
2024-10-25 19:30:05 +0000

    Formatting changes

--- tests/test_get_least_likely_features.py
+++ tests/test_get_least_likely_features.py
@@ -1,23 +1,28 @@
 import numpy as np
+import pytest
+
+from dataeval._internal.metrics.metadata_least_likely import get_least_likely_features
+
 
 @pytest.fixture
 def mock_llf():
-    md0 = {'time': [1.2, 3.4, 5.6], 'altitude': [235, 6789, 101112]}
-    md1 = {'time': [7.8, 9.10, 11.12], 'altitude': [532, 9876, 211101]}
+    md0 = {"time": [1.2, 3.4, 5.6], "altitude": [235, 6789, 101112]}
+    md1 = {"time": [7.8, 9.10, 11.12], "altitude": [532, 9876, 211101]}
 
     is_ood = np.array([False, False, True])
 
     llf = get_least_likely_features(md0, md1, is_ood)
     return llf
 
-class TestGetLeastLikelyFeatures():
+
+class TestGetLeastLikelyFeatures:
     def test_nothing(self, mock_llf):
-        features = mock_llf
+        _ = mock_llf
         assert True
 
     def test_llf(self, mock_llf):
         llf = mock_llf
-        assert llf[0] == 'time'
+        assert llf[0] == "time"
 
     def test_llf_type(self, mock_llf):
         ml = mock_llf
--- tests/test_get_metadata_ood_mi.py
+++ tests/test_get_metadata_ood_mi.py
@@ -1,20 +1,21 @@
 import numpy as np
-
 import pytest
 
 from dataeval._internal.metrics.metadata_ood_mi import get_metadata_ood_mi
 
 pytest.fixture
+
+
 def mock_md_ood():
     rng = np.random.default_rng(20241022)
     nsamp, nfeatures = 100, 5
     x = rng.normal(size=(nsamp, nfeatures))
     # features 0, 1, and 2 should have some MI, but 3 and 4 will not
-    is_ood = np.abs(x[:, 0]) > np.abs(x[:,1]) + np.abs(x[:,2])
+    is_ood = np.abs(x[:, 0]) > np.abs(x[:, 1]) + np.abs(x[:, 2])
 
     md = {}
     for i in range(nfeatures):
-        md.update({'feature_'+str(i): x[:,i]})
+        md.update({"feature_" + str(i): x[:, i]})
 
     discrete_features = False
 
@@ -22,11 +23,11 @@ def mock_md_ood():
 
     return mi_dict
 
-class TestGetMetadataOodMi():
+
+class TestGetMetadataOodMi:
     def test_type(self, mock_md_ood):
         assert isinstance(mock_md_ood, dict)
 
     def test_mi_values(self, mock_md_ood):
         mi_dict = mock_md_ood
-        assert np.allclose([v for v in mi_dict.values()], [0.17562533, 0.18743624, 0.15217528, 0.        , 0.        ])
-
+        assert np.allclose(list(mi_dict.values()), [0.17562533, 0.18743624, 0.15217528, 0.0, 0.0])
