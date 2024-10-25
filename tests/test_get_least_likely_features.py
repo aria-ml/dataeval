2c45904e0fe0972ec4c5fa52b4c74454e56a3a07
Bill Peria <bill.peria@ariacoustics.com>
2024-10-25 17:27:32 +0000

    Added 3 py files to tests folder, for get_metadata_ood_mi(), meta_distribution_compare(), and get_least_liekly_features()

---
+++ tests/test_get_least_likely_features.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+@pytest.fixture
+def mock_llf():
+    md0 = {'time': [1.2, 3.4, 5.6], 'altitude': [235, 6789, 101112]}
+    md1 = {'time': [7.8, 9.10, 11.12], 'altitude': [532, 9876, 211101]}
+
+    is_ood = np.array([False, False, True])
+
+    llf = get_least_likely_features(md0, md1, is_ood)
+    return llf
+
+class TestGetLeastLikelyFeatures():
+    def test_nothing(self, mock_llf):
+        features = mock_llf
+        assert True
+
+    def test_llf(self, mock_llf):
+        llf = mock_llf
+        assert llf[0] == 'time'
+
+    def test_llf_type(self, mock_llf):
+        ml = mock_llf
+        assert isinstance(ml, np.ndarray)
---
+++ tests/test_meta_distribution_compare.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+from dataeval._internal.metrics.metadata_ks_compare import meta_distribution_compare
+
+
+@pytest.fixture
+def mock_mdc():
+    md0 = {'time': [1.2, 3.4, 5.6], 'altitude': [235, 6789, 101112]}
+    md1 = {'time': [7.8, 9.10, 11.12], 'altitude': [532, 9876, 211101]}
+    mdc = meta_distribution_compare(md0, md1)
+    return mdc
+
+class TestMetadataCompare():
+    # test conditions, as many as you can think of
+    def test_type(self, mock_mdc):
+        mdc = mock_mdc
+        assert isinstance(mdc, dict) 
+    
+    def test_shifts(self, mock_mdc):
+        mdc = mock_mdc
+        assert mdc['time'].shift_magnitude == 2.7 and  np.isclose(mdc['altitude'].shift_magnitude,  0.7492490855199898)
+
+    def test_pvalue(self, mock_mdc):
+        mdc = mock_mdc
+        assert np.isclose(mdc['time'].pvalue, 0.0) and np.isclose(mdc['altitude'].pvalue, 0.9444444444444444)
+
