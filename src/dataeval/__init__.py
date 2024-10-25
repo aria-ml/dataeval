97ffdd586ddd5dddcbef3ef761e55fb3507fd9f9
Andrew Weng <andrew.weng@ariacoustics.com>
2024-10-25 11:07:01 -0700

    Make optional checks more granular

--- src/dataeval/__init__.py
+++ src/dataeval/__init__.py
@@ -3,6 +3,7 @@ __version__ = "0.0.0"
 from importlib.util import find_spec
 
 _IS_TORCH_AVAILABLE = find_spec("torch") is not None
+_IS_TORCHVISION_AVAILABLE = find_spec("torchvision") is not None
 _IS_TENSORFLOW_AVAILABLE = find_spec("tensorflow") is not None and find_spec("tensorflow_probability") is not None
 
 del find_spec
--- src/dataeval/utils/torch/__init__.py
+++ src/dataeval/utils/torch/__init__.py
@@ -5,8 +5,17 @@ While these metrics can take in custom models, DataEval provides utility classes
 to create a seamless integration between custom models and DataEval's metrics.
 """
 
+from dataeval import _IS_TORCH_AVAILABLE, _IS_TORCHVISION_AVAILABLE
 from dataeval._internal.utils import read_dataset
 
-from . import models, trainer
+__all__ = []
 
-__all__ = ["read_dataset", "models", "trainer"]
+if _IS_TORCH_AVAILABLE:
+    from . import models, trainer
+
+    __all__ += ["read_dataset", "models", "trainer"]
+
+if _IS_TORCHVISION_AVAILABLE:
+    from . import datasets
+
+    __all__ += ["datasets"]
