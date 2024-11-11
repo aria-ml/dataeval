55b4f5ca0e1b31bcadbd6b8b8f4d489ecde8224a
Bill Peria <bill.peria@ariacoustics.com>
2024-11-11 22:22:51 +0000

    Several temporary changes exploring vanishing gradients and OOD blindness

--- src/dataeval/models/torch/trainer.py
+++ src/dataeval/models/torch/trainer.py
@@ -64,9 +64,10 @@ def trainer(
     # THIS WILL NEED MORE THAN JUST TYPO CHANGES!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     #
     # if optimizer is None:
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    # optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-2)
 
-    loss_fn = loss_fn() if isinstance(loss_fn, type) else loss_fn
+    # loss_fn = loss_fn() if isinstance(loss_fn, type) else loss_fn
     # optimizer = optimizer() if isinstance(optimizer, type) else optimizer
 
     # train_data = x_train if y_train is None else (x_train, y_train)
@@ -81,40 +82,59 @@ def trainer(
     # dataset = dataset.shuffle(buffer_size=buffer_size).batch(batch_size)
     # n_minibatch = len(dataset)
 
+    # def crapnorm(xraw):
+    #     return (xraw - torch.tensor(0.5)) * torch.tensor(2.0)
+
+    # preprocess_fn = crapnorm
+
     loader = DataLoader(dataset=dataset)
     # iterate over epochs
     for epoch in range(epochs):
         print(f"Epoch {epoch}...")
+        first_time_in_epoch = True
+
         for step, data in enumerate(loader):
             x, y = [d.to(torch.float32) for d in data] if len(data) > 1 else (data[0].to(torch.float32), None)
 
             if isinstance(preprocess_fn, Callable):
                 x = preprocess_fn(x)
 
-            y_hat = model(x)
+            y_hat = model(x)  # .clone().detach().requires_grad_(True)
             y = x if y is None else y
-            if isinstance(loss_fn, Callable):
-                args = [y] + list(y_hat) if isinstance(y_hat, tuple) else [y, y_hat]
-                loss = loss_fn(*args)
-            else:
-                loss = cast(torch.Tensor, torch.tensor(0.0, dtype=torch.float32))
+            # y = y.clone().detach().requires_grad_(True)
+
+            loss = loss_fn(y, y_hat)  # type: ignore
+
+            # if isinstance(loss_fn, Callable):
+            #     args = [y] + list(y_hat) if isinstance(y_hat, tuple) else [y, y_hat]
+            #     loss = loss_fn(*args)
+            # else:
+            #     loss = cast(torch.Tensor, torch.tensor(0.0, dtype=torch.float32))
             # if model.losses:  # additional model losses
             #     loss = cast(torch.Tensor, torch.add(sum(model.losses), loss))
 
             optimizer.zero_grad()
-            # loss = cast(torch.Tensor, torch.add(reg_loss_fn(model), loss))  # alternative way they might be specified
             loss.backward()
+
+            total_norm = 0
+            parameters = [p for p in model.parameters() if p.grad is not None and p.requires_grad]
+            for p in parameters:
+                param_norm = p.grad.detach().data.norm(2)  # type: ignore
+                total_norm += param_norm.item() ** 2
+            total_norm = total_norm**0.5
+
+            psave = list(model.parameters())[0].clone().detach().numpy()
             optimizer.step()
+            pnew = list(model.parameters())[0].clone().detach().numpy()
 
-            if step == 2:
-                psave = list(model.parameters())[0].clone().detach().numpy()
-
-            if step == 3:
-                pnew = list(model.parameters())[0].clone().detach().numpy()
-                if (psave == pnew).all():  # type: ignore
-                    print("parameters not changing")
-                    pass
+            if (psave == pnew).all() and first_time_in_epoch:  # type: ignore
+                first_time_in_epoch = False
+                print(f"epoch: {epoch}, step: {step}, parameters not changing")
+                pass
+            else:
+                # print(total_norm)
+                pass
 
             if step % 500 == 0:
-                print(loss)
+                print(f"loss: {loss}, |grad|: {total_norm}")
         pass
