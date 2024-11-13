43bfdf7be32435b0b325f630e424aac8b61696fa
Andrew Weng <andrew.weng@ariacoustics.com>
2024-11-13 14:08:36 -0800

    Fix plot function for junk case and add deps test

--- tests/test_mindeps.py
+++ tests/test_mindeps.py
@@ -1,11 +1,23 @@
+from importlib.util import find_spec
+
 import numpy as np
+import pytest
 
 from dataeval.detectors.drift import DriftCVM, DriftKS
 from dataeval.detectors.linters import Duplicates, Outliers
 from dataeval.interop import to_numpy
-from dataeval.metrics.bias import label_parity
+from dataeval.metrics.bias import coverage
+from dataeval.metrics.bias.parity import label_parity
 from dataeval.metrics.estimators import ber, divergence, uap
 from dataeval.metrics.stats import dimensionstats, hashstats, pixelstats, visualstats
+
+
+class NullContext:
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        pass
 
 
 class TestMinimalDependencies:
@@ -42,6 +54,10 @@ class TestMinimalDependencies:
 
     def testBias(self):
         label_parity(self.labels, self.labels)
+        output = coverage(self.images)
+
+        with pytest.raises(ImportError) if not find_spec("matplotlib") else NullContext():
+            output.plot(self.images, 1)
 
     def testToNumpy(self):
         actual = to_numpy([[1, 2], [3, 4]])  # type: ignore
