192671ed7a1039af2845ae77f5535266b3e326f6
Bill Peria <bill.peria@ariacoustics.com>
2024-11-14 00:38:03 +0000

    Type hints in gmm.py, realized there's no reason to carry numpy return values from functions.

--- src/dataeval/torch/models/gmm.py
+++ src/dataeval/torch/models/gmm.py
@@ -121,6 +121,6 @@ def gmm_energy(
 
 
 # replace tf.expand with this....
-def expand(x: NDArray[Any] | torch.Tensor, dim=None) -> NDArray[Any] | torch.Tensor:
+def expand(x: NDArray[Any] | torch.Tensor, dim=None) -> torch.Tensor:
     newshape = (*x.shape, 1) if dim is None else (*x.shape[0:dim], 1, *x.shape[dim:])
-    return x.reshape(newshape)
+    return torch.tensor(x).reshape(newshape)
