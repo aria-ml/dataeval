9efc5df43d2122f4316df520a2005477e4ee393a
Max Bright <max.bright@ariacoustics.com>
2024-11-21 10:49:20 -0800

    Added test

--- tests/metrics/test_metadata_binning.py
+++ tests/metrics/test_metadata_binning.py
@@ -10,3 +10,10 @@ class TestMDBinningFunctional:
         output = metadata_binning(factors, bincounts)
         disc_factors = output.discrete
         assert len(np.unique(disc_factors["data1"])) == 2
+
+    def test_bin_edges(self):
+        factors = [{"data1": [0.1, 1.1, 1.2]}]
+        bin_edges = {"data1": [-np.inf, 1, np.inf]}
+        output = metadata_binning(factors, bin_edges)
+        disc_factors = output.discrete
+        assert len(np.unique(disc_factors["data1"])) == 2
