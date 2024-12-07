279dba1c45410d0cf9051f60c471445764b63bdf
Andrew Weng <andrew.weng@ariacoustics.com>
2024-12-07 01:17:19 -0800

    Fix torch gmm functions and enable tests

--- src/dataeval/utils/torch/gmm.py
+++ src/dataeval/utils/torch/gmm.py
@@ -10,11 +10,8 @@ Licensed under Apache Software License (Apache 2.0)
 
 from __future__ import annotations
 
-from typing import Any
-
 import numpy as np
 import torch
-from numpy.typing import NDArray
 
 from dataeval.utils.gmm import GaussianMixtureModelParams
 
@@ -41,18 +38,20 @@ def gmm_params(z: torch.Tensor, gamma: torch.Tensor) -> GaussianMixtureModelPara
     sum_gamma = torch.sum(gamma, 0)  # K
     phi = sum_gamma / N  # K
     # K x D (D = latent_dim)
-    mu = torch.sum(gamma.reshape((*gamma.shape, 1)) * z.reshape(*z.shape, 1), 0) / sum_gamma.reshape(*sum_gamma.shape)
-    z_mu = expand(z, 1) - expand(mu, 0)  # N x K x D
-    z_mu_outer = expand(z_mu, -1) * expand(z_mu, -2)  # N x K x D x D
+    mu = torch.sum(torch.unsqueeze(gamma, -1) * torch.unsqueeze(z, 1), 0) / torch.unsqueeze(sum_gamma, -1)
+    z_mu = torch.unsqueeze(z, 1) - torch.unsqueeze(mu, 0)  # N x K x D
+    z_mu_outer = torch.unsqueeze(z_mu, -1) * torch.unsqueeze(z_mu, -2)  # N x K x D x D
 
     # K x D x D
-    cov = torch.sum((gamma.reshape((*gamma.shape, 1, 1)) * z_mu_outer, 0) / gamma.reshape((*gamma.shape, 1, 1)), -1)
+    cov = torch.sum(torch.unsqueeze(torch.unsqueeze(gamma, -1), -1) * z_mu_outer, 0) / torch.unsqueeze(
+        torch.unsqueeze(sum_gamma, -1), -1
+    )
 
     # cholesky decomposition of covariance and determinant derivation
-    D = torch.shape(cov)[1]  # type: ignore
+    D = cov.shape[1]
     eps = 1e-6
     L = torch.linalg.cholesky(cov + torch.eye(D) * eps)  # K x D x D
-    log_det_cov = 2.0 * torch.sum(torch.log(torch.diagonal(L)), 1)  # K
+    log_det_cov = 2.0 * torch.sum(torch.log(torch.diagonal(L, dim1=-2, dim2=-1)), 1)  # K
 
     return GaussianMixtureModelParams(phi, mu, cov, L, log_det_cov)
 
@@ -79,16 +78,14 @@ def gmm_energy(
     cov_diag
         The inverse sum of the diagonal components of the covariance matrix.
     """
-    D = torch.shape(params.cov)[1]  # type: ignore
-    z_mu = expand(z) - expand(params.mu, 0)  # N x K x D
+    D = params.cov.shape[1]
+    z_mu = torch.unsqueeze(z, 1) - torch.unsqueeze(params.mu, 0)  # N x K x D
     z_mu_T = torch.permute(z_mu, dims=[1, 2, 0])  # K x D x N
-    v = torch.triangular_solve(z_mu_T, params.L, upper=False)  # K x D x D
+    v = torch.linalg.solve_triangular(params.L, z_mu_T, upper=False)  # K x D x D
 
     # rewrite sample energy in logsumexp format for numerical stability
-    logits = torch.log(expand(params.phi)) - 0.5 * (
-        torch.sum(torch.square(v), 1)  # type: ignore
-        + torch.cast(D, torch.float32) * torch.math.log(2.0 * np.pi)  # type: ignore py38
-        + expand(params.log_det_cov)
+    logits = torch.log(torch.unsqueeze(params.phi, -1)) - 0.5 * (
+        torch.sum(torch.square(v), 1) + float(D) * np.log(2.0 * np.pi) + torch.unsqueeze(params.log_det_cov, -1)
     )  # K x N
     sample_energy = -torch.logsumexp(logits, 0)  # N
 
@@ -96,12 +93,6 @@ def gmm_energy(
         sample_energy = torch.mean(sample_energy)
 
     # inverse sum of variances
-    cov_diag = torch.sum(torch.divide(torch.tensor(1), torch.diag(params.cov)))
+    cov_diag = torch.sum(torch.divide(torch.tensor(1), torch.diagonal(params.cov, dim1=-2, dim2=-1)))
 
     return sample_energy, cov_diag
-
-
-# replace tf.expand with this....
-def expand(x: NDArray[Any] | torch.Tensor, dim: int | None = None) -> torch.Tensor:
-    newshape = (*x.shape, 1) if dim is None else (*x.shape[0:dim], 1, *x.shape[dim:])
-    return torch.tensor(x).reshape(newshape)
