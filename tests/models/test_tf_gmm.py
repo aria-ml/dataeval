6bd20653a7686997b1e10e8743758c036a6c7802
Andrew Weng <andrew.weng@ariacoustics.com>
2024-12-07 01:17:19 -0800

    Fix torch gmm functions and enable tests

--- tests/models/test_tf_gmm.py
+++ tests/models/test_tf_gmm.py
@@ -9,11 +9,10 @@ Licensed under Apache Software License (Apache 2.0)
 import numpy as np
 import pytest
 import tensorflow as tf
+import torch
 
 import dataeval.utils.tensorflow._internal.gmm as tensorflow_gmm
-
-# import torch
-# import dataeval.utils.torch.gmm as torch_gmm
+import dataeval.utils.torch.gmm as torch_gmm
 
 N, K, D = 10, 5, 1
 z = np.random.rand(N, D).astype(np.float32)
@@ -24,11 +23,11 @@ gamma = np.random.rand(N, K).astype(np.float32)
     "module, tz, tg",
     [
         (tensorflow_gmm, tf.convert_to_tensor(z), tf.convert_to_tensor(gamma)),
-        # (torch_gmm, torch.from_numpy(z), torch.from_numpy(gamma)),
+        (torch_gmm, torch.from_numpy(z), torch.from_numpy(gamma)),
     ],
 )
 def test_gmm_params_energy(module, tz, tg):
-    params = module.gmm_params(z, gamma)
+    params = module.gmm_params(tz, tg)
     assert params.phi.numpy().shape[0] == K == params.log_det_cov.shape[0]  # type: ignore
     assert params.mu.numpy().shape == (K, D)  # type: ignore
     assert params.cov.numpy().shape == params.L.numpy().shape == (K, D, D)  # type: ignore
@@ -36,8 +35,8 @@ def test_gmm_params_energy(module, tz, tg):
         assert (np.diag(params.cov[_].numpy()) >= 0.0).all()  # type: ignore
         assert (np.diag(params.L[_].numpy()) >= 0.0).all()  # type: ignore
 
-    sample_energy, cov_diag = module.gmm_energy(z, params, return_mean=True)
+    sample_energy, cov_diag = module.gmm_energy(tz, params, return_mean=True)
     assert sample_energy.numpy().shape == cov_diag.numpy().shape == ()  # type: ignore
 
-    sample_energy, _ = module.gmm_energy(z, params, return_mean=False)
+    sample_energy, _ = module.gmm_energy(tz, params, return_mean=False)
     assert sample_energy.numpy().shape[0] == N  # type: ignore
