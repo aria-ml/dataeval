f34275ec07e75a221d818a7b08ca9da226abd9a7
Robert Jullens <shaun.jullens@ariacoustics.com>
2024-12-20 22:34:06 +0000

    Update dataset split unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
 
-from itertools import product
 from zipfile import ZipFile
 
 import numpy as np
 import pytest
-from numpy.typing import NDArray
 
 import dataeval.metrics.stats.base as base
 
@@ -13,6 +11,9 @@ TEMP_CONTENTS = "ABCDEF1234567890"
 
 # Temporarily set DEFAULT_PROCESSES to 1
 base.DEFAULT_PROCESSES = 1
+
+
+RNG = np.random.default_rng(0)
 
 
 @pytest.fixture(scope="session")
@@ -64,33 +65,3 @@ def mnist_npy(tmp_path_factory):
     np.save(mnistc_temp / "train_images.npy", train, allow_pickle=False)
     np.save(mnistc_temp / "train_labels.npy", labels, allow_pickle=False)
     yield temp
-
-
-@pytest.fixture(scope="session")
-def labels_with_metadata() -> tuple[NDArray, dict[str, NDArray]]:
-    """Test fixture that returns multiclass labels, as well as a metadata dictionary containing
-    columns with various testable properties.
-
-    Specifically, the metadata columns contain all possible combinations of:
-    discrete vs. continuous vs. angles,
-    correlated vs. uncorrelated with labels,
-
-
-    Each metadata dictionary key reflects the properties of its corresponding feature vector.
-    For example, the key 'CorrelatedAngle' indexes into a column of angle data correlated with
-    labels
-    """
-    rng = np.random.default_rng(9251990)
-    n_labels = 5000
-    labels = rng.integers(low=0, high=5, size=(n_labels,))
-    metadata = {}
-    metadata_attrs = [["Discrete", "Continuous", "Angle"], ["Correlated", "Uncorrelated"]]
-    md_keys = ["".join(prod) for prod in product(*metadata_attrs)]
-    for key in md_keys:
-        md = rng.uniform(-10, 10, size=(n_labels,)) if "Uncorrelated" in key else rng.uniform(-10, 10, size=(n_labels,))
-        if "Angle" in key:
-            md = ((md + 10) / 20) * 2 * np.pi
-        elif "Discrete" in key:
-            md = int(np.round(md))
-        metadata[key] = md
-    return labels, metadata
