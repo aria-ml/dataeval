d09f7476e0df35ce1eacbe89e854e2721df60b9a
Andrew Weng <andrew.weng@ariacoustics.com>
2025-03-26 13:57:31 -0700

    Remove prototype folder - see dataeval-prototype repository for new home

--- prototype/torchmetrics/uap_metric.py
+++
@@ -1,6 +0,0 @@
-from torchmetrics.classification.average_precision import MulticlassAveragePrecision
-
-
-class UAPMetric(MulticlassAveragePrecision):
-    def __init__(self, num_classes: int):
-        super().__init__(num_classes=num_classes, average="weighted")
