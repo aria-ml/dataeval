261a93f0214e9c80e6c4610515db8d38faba1add
Andrew Weng <andrew.weng@ariacoustics.com>
2025-04-03 19:43:30 +0000

    Integrate pruning prioritization

--- src/dataeval/utils/data/selections/__init__.py
+++ src/dataeval/utils/data/selections/__init__.py
@@ -4,6 +4,7 @@ __all__ = [
     "ClassFilter",
     "Indices",
     "Limit",
+    "Prioritize",
     "Reverse",
     "Shuffle",
 ]
@@ -11,5 +12,6 @@ __all__ = [
 from dataeval.utils.data.selections._classfilter import ClassFilter
 from dataeval.utils.data.selections._indices import Indices
 from dataeval.utils.data.selections._limit import Limit
+from dataeval.utils.data.selections._prioritize import Prioritize
 from dataeval.utils.data.selections._reverse import Reverse
 from dataeval.utils.data.selections._shuffle import Shuffle
