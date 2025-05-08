Fix stats calculation for ratios and update box_count to object_count

src/dataeval/metrics/stats/_base.py
@@ -117,7 +117,7 @@ class StatsProcessor(Generic[TStatsOutput]):
 
     @classmethod
     def convert_output(
-        cls, source: dict[str, Any], source_index: list[SourceIndex], box_count: list[int]
+        cls, source: dict[str, Any], source_index: list[SourceIndex], object_count: list[int]
     ) -> TStatsOutput:
         output = {}
         attrs = dict(ChainMap(*(getattr(c, "__annotations__", {}) for c in cls.output_class.__mro__)))
@@ -128,7 +128,8 @@ class StatsProcessor(Generic[TStatsOutput]):
                 output[key] = np.asarray(source[key], dtype=np.dtype(dtype_match.group(1)))
             else:
                 output[key] = source[key]
-        return cls.output_class(**output, source_index=source_index, box_count=np.asarray(box_count, dtype=np.uint16))
+        np_object_count = np.asarray(object_count, dtype=np.uint16)
+        return cls.output_class(**output, source_index=source_index, object_count=np_object_count)
 
 
 @dataclass
@@ -226,7 +227,7 @@ def run_stats(
     """
     results_list: list[dict[str, NDArray[np.float64]]] = []
     source_index: list[SourceIndex] = []
-    box_count: list[int] = []
+    object_count: list[int] = []
 
     warning_list = []
     stats_processor_cls = stats_processor_cls if isinstance(stats_processor_cls, Iterable) else [stats_processor_cls]
@@ -245,7 +246,7 @@ def run_stats(
         ):
             results_list.extend(r.results)
             source_index.extend(r.source_indices)
-            box_count.extend(r.box_counts)
+            object_count.extend(r.box_counts)
             warning_list.extend(r.warnings_list)
 
     # warnings are not emitted while in multiprocessing pools so we emit after gathering all warnings
@@ -260,7 +261,7 @@ def run_stats(
             else:
                 output.setdefault(stat, []).append(result.tolist() if isinstance(result, np.ndarray) else result)
 
-    outputs = [s.convert_output(output, source_index, box_count) for s in stats_processor_cls]
+    outputs = [s.convert_output(output, source_index, object_count) for s in stats_processor_cls]
     return outputs
 
 

src/dataeval/metrics/stats/_boxratiostats.py
@@ -8,8 +8,9 @@ from typing import Any, Callable, Generic, TypeVar, cast
 import numpy as np
 from numpy.typing import NDArray
 
+from dataeval.config import EPSILON
 from dataeval.outputs._base import set_metadata
-from dataeval.outputs._stats import BOX_COUNT, SOURCE_INDEX, BaseStatsOutput, DimensionStatsOutput
+from dataeval.outputs._stats import OBJECT_COUNT, SOURCE_INDEX, BaseStatsOutput
 
 TStatOutput = TypeVar("TStatOutput", bound=BaseStatsOutput, contravariant=True)
 ArraySlice = tuple[int, int]
@@ -40,16 +41,12 @@ class BoxImageStatsOutputSlice(Generic[TStatOutput]):
         self.img = self.StatSlicer(img_stats, img_slice)
 
 
-RATIOSTATS_OVERRIDE_MAP: dict[type, dict[str, Callable[..., NDArray[Any]]]] = {
-    DimensionStatsOutput: dict[str, Callable[[BoxImageStatsOutputSlice[DimensionStatsOutput]], NDArray[Any]]](
-        {
-            "left": lambda x: x.box["left"] / x.img["width"],
-            "top": lambda x: x.box["top"] / x.img["height"],
-            "channels": lambda x: x.box["channels"],
-            "depth": lambda x: x.box["depth"],
-            "distance": lambda x: x.box["distance"],
-        }
-    ),
+RATIOSTATS_OVERRIDE_MAP: dict[str, Callable[[BoxImageStatsOutputSlice[Any]], NDArray[Any]]] = {
+    "left": lambda x: x.box["left"] / x.img["width"],
+    "top": lambda x: x.box["top"] / x.img["height"],
+    "channels": lambda x: x.box["channels"],
+    "depth": lambda x: x.box["depth"],
+    "distance": lambda x: x.box["distance"],
 }
 
 
@@ -72,7 +69,7 @@ def calculate_ratios(key: str, box_stats: BaseStatsOutput, img_stats: BaseStatsO
     # Copy over stats index maps and box counts
     if key in (SOURCE_INDEX):
         return copy.deepcopy(stats)
-    elif key == BOX_COUNT:
+    elif key == OBJECT_COUNT:
         return np.copy(stats)
 
     # Calculate ratios for each stat
@@ -84,10 +81,9 @@ def calculate_ratios(key: str, box_stats: BaseStatsOutput, img_stats: BaseStatsO
         box_j = len(box_stats) if i == len(box_map) - 1 else box_map[i + 1]
         img_j = len(img_stats) if i == len(img_map) - 1 else img_map[i + 1]
         stats = BoxImageStatsOutputSlice(box_stats, (box_i, box_j), img_stats, (img_i, img_j))
-        out_type = type(box_stats)
-        use_override = out_type in RATIOSTATS_OVERRIDE_MAP and key in RATIOSTATS_OVERRIDE_MAP[out_type]
+        use_override = key in RATIOSTATS_OVERRIDE_MAP
         with np.errstate(divide="ignore", invalid="ignore"):
-            ratio = RATIOSTATS_OVERRIDE_MAP[out_type][key](stats) if use_override else stats.box[key] / stats.img[key]
+            ratio = RATIOSTATS_OVERRIDE_MAP[key](stats) if use_override else stats.box[key] / (stats.img[key] + EPSILON)
         out_stats[box_i:box_j] = ratio.reshape(-1, *out_stats[box_i].shape)
     return out_stats
 

src/dataeval/outputs/_stats.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 __all__ = []
 
 from dataclasses import dataclass
-from typing import Any, Iterable, NamedTuple, Optional, Union
+from typing import TYPE_CHECKING, Any, Iterable, NamedTuple, Optional, Sequence, Union
 
 import numpy as np
 import pandas as pd
@@ -13,10 +13,13 @@ from typing_extensions import TypeAlias
 from dataeval.outputs._base import Output
 from dataeval.utils._plot import channel_histogram_plot, histogram_plot
 
+if TYPE_CHECKING:
+    from matplotlib.figure import Figure
+
 OptionalRange: TypeAlias = Optional[Union[int, Iterable[int]]]
 
 SOURCE_INDEX = "source_index"
-BOX_COUNT = "box_count"
+OBJECT_COUNT = "object_count"
 
 
 class SourceIndex(NamedTuple):
@@ -51,15 +54,16 @@ class BaseStatsOutput(Output):
     ----------
     source_index : List[SourceIndex]
         Mapping from statistic to source image, box and channel index
-    box_count : NDArray[np.uint16]
+    object_count : NDArray[np.uint16]
+        The number of detected objects in each image
     """
 
     source_index: list[SourceIndex]
-    box_count: NDArray[np.uint16]
+    object_count: NDArray[np.uint16]
 
     def __post_init__(self) -> None:
         length = len(self.source_index)
-        bad = {k: len(v) for k, v in self.data().items() if k not in [SOURCE_INDEX, BOX_COUNT] and len(v) != length}
+        bad = {k: len(v) for k, v in self.data().items() if k not in [SOURCE_INDEX, OBJECT_COUNT] and len(v) != length}
         if bad:
             raise ValueError(f"All values must have the same length as source_index. Bad values: {str(bad)}.")
 
@@ -123,11 +127,27 @@ class BaseStatsOutput(Output):
 
         return max_channels, ch_mask
 
-    def factors(self, include_empty: bool = True) -> dict[str, NDArray[Any]]:
+    def factors(self, filter: str | Sequence[str] | None = None, include_empty: bool = True) -> dict[str, NDArray[Any]]:  # noqa: A002
+        """
+        Returns all 1-dimensional data as a dictionary of numpy arrays.
+
+        Parameters
+        ----------
+        filter : str, Sequence[str] or None, default None:
+            If provided, only returns keys that match the filter.
+        include_empty : bool, default True
+            If True, include arrays that are all zeros.
+
+        Returns
+        -------
+        dict[str, NDArray[Any]]
+        """
+        _filter = [filter] if isinstance(filter, str) else filter
         return {
             k: v
             for k, v in self.data().items()
-            if k not in (SOURCE_INDEX, BOX_COUNT)
+            if k not in (SOURCE_INDEX, OBJECT_COUNT)
+            and (_filter is None or k in _filter)
             and isinstance(v, np.ndarray)
             and (include_empty or v[v != 0].size > 0)
             and v.ndim == 1
@@ -135,12 +155,28 @@ class BaseStatsOutput(Output):
 
     def plot(
         self, log: bool, channel_limit: int | None = None, channel_index: int | Iterable[int] | None = None
-    ) -> None:
+    ) -> Figure:
+        """
+        Plots the statistics as a set of histograms.
+
+        Parameters
+        ----------
+        log : bool
+            If True, plots the histograms on a logarithmic scale.
+        channel_limit : int or None
+            The maximum number of channels to plot. If None, all channels are plotted.
+        channel_index : int, Iterable[int] or None
+            The index or indices of the channels to plot. If None, all channels are plotted.
+
+        Returns
+        -------
+        matplotlib.Figure
+        """
         max_channels, ch_mask = self._get_channels(channel_limit, channel_index)
         if max_channels == 1:
-            histogram_plot(self.factors(), log)
+            return histogram_plot(self.factors(), log)
         else:
-            channel_histogram_plot(self.factors(), log, max_channels, ch_mask)
+            return channel_histogram_plot(self.factors(), log, max_channels, ch_mask)
 
 
 @dataclass(frozen=True)

tests/metrics/test_stats.py
@@ -460,7 +460,7 @@ class TestBaseStatsOutput:
                 foo=[1, 2, 3, 4],
                 bar=["a", "b", "c"],
                 source_index=[1, 2, 3, 4],  # type: ignore
-                box_count=np.array([1, 2, 3, 4]),
+                object_count=np.array([1, 2, 3, 4]),
             )
 
 