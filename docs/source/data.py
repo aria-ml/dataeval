1460821efc37613867044ffbca640aee0e9e3085
Andrew Weng <andrew.weng@ariacoustics.com>
2025-05-23 03:46:23 +0000

    Restructure notebooks in documentation

--- docs/source/data.py
+++ docs/source/data.py
@@ -20,21 +20,7 @@ def cd(rel_path: str):
 def download():
     from dataeval.utils.datasets import CIFAR10, MNIST, VOCDetection
 
-    with cd("how_to"):
-        # AETrainerTutorial.ipynb
-        # BayesErrorRateEstimationTutorial.ipynb
-        # ClassLearningCurvesTutorial.ipynb
-        # ClassLabelAnalysisTutorial.ipynb
-        # DuplicatesTutorial.ipynb
-        # HPDivergenceTutorial.ipynb
+    with cd("notebooks"):
         MNIST(root="./data", download=True, image_set="train", corruption="translate")
-
-        # LintingTutorial.ipynb
         CIFAR10(root="./data", download=True, image_set="test")
-
-    with cd("tutorials"):
-        # EDA_Part1.ipynb
-        # EDA_Part2.ipynb
-        # EDA_Part3.ipynb
-        # Data_Monitoring.ipynb
         VOCDetection("./data", download=True, image_set="train", year="2011")
