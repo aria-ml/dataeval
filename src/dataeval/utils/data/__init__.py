1e9890b47ea4a598f7690825084f6a6737e40c7c
Andrew Weng <andrew.weng@ariacoustics.com>
2025-06-12 21:36:49 +0000

    Implement dataset validation function

--- src/dataeval/utils/data/__init__.py
+++ src/dataeval/utils/data/__init__.py
@@ -2,10 +2,12 @@
 
 from dataeval.utils.data import collate, metadata
 from dataeval.utils.data._dataset import to_image_classification_dataset, to_object_detection_dataset
+from dataeval.utils.data._validate import validate_dataset
 
 __all__ = [
     "collate",
     "metadata",
     "to_image_classification_dataset",
     "to_object_detection_dataset",
+    "validate_dataset",
 ]
