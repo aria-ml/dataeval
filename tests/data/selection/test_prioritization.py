0c4502d362b262f2ef594aac4997c62160215229
Jonathan Botts <jonathan.botts@ariacoustics.com>
2025-06-12 02:23:51 +0000

    Integrate prioritization re-ranking policies

--- tests/data/selection/test_prioritization.py
+++ tests/data/selection/test_prioritization.py
@@ -120,15 +120,15 @@ class TestPrioritizeSelection:
         return Embeddings(self.get_dataset(n), batch_size=self.batch_size, model=self.model, device=self.device)
 
     @pytest.mark.parametrize(
-        "method, method_kwargs",
+        "method, policy, method_kwargs",
         (
-            ("knn", {"k": 10}),
-            ("kmeans_distance", {"c": 100}),
-            ("kmeans_complexity", {"c": 100}),
+            ("knn", "hard_first", {"k": 10}),
+            ("kmeans_distance", "hard_first", {"c": 100}),
+            ("kmeans_complexity", "hard_first", {"c": 100}),
         ),
     )
-    def test_prioritize_init(self, method, method_kwargs):
-        p = Prioritize(self.model, self.batch_size, self.device, method, **method_kwargs)
+    def test_prioritize_init(self, method, policy, method_kwargs):
+        p = Prioritize(self.model, self.batch_size, self.device, method, policy, **method_kwargs)
         assert p._method == method
         assert (p._c == method_kwargs["c"]) if "c" in method_kwargs else (p._c is None)
         assert (p._k == method_kwargs["k"]) if "k" in method_kwargs else (p._k is None)
@@ -137,9 +137,67 @@ class TestPrioritizeSelection:
         p(dataset)
         assert any(i != j for i, j in zip(dataset._selection, range(1000)))
 
+    @pytest.mark.parametrize(
+        "method, policy, policy_kwargs",
+        (
+            ("knn", "hard_first", {"k": 10}),
+            ("knn", "easy_first", {"k": 10}),
+            ("knn", "stratified", {"k": 10}),
+            ("knn", "class_balance", {"k": 10, "class_label": np.random.randint(low=0, high=10, size=1000)}),
+        ),
+    )
+    def test_prioritize_init_with_correct_policy_params(self, method, policy, policy_kwargs):
+        p = Prioritize(self.model, self.batch_size, self.device, method, policy, **policy_kwargs)
+        assert p._policy == policy
+        assert p._method == method
+        assert (p._c == policy_kwargs["c"]) if "c" in policy_kwargs else (p._c is None)
+        assert (p._k == policy_kwargs["k"]) if "k" in policy_kwargs else (p._k is None)
+        assert p._get_sorter(1000) is not None
+        dataset = self.get_dataset()
+        p(dataset)
+        assert any(i != j for i, j in zip(dataset._selection, range(1000)))
+
     def test_prioritize_invalid_method_raises_valueerror(self):
         with pytest.raises(ValueError):
-            Prioritize(self.model, self.batch_size, self.device, "invalid")  # type: ignore
+            Prioritize(self.model, self.batch_size, self.device, "invalid", "hard_first")  # type: ignore
+
+    def test_prioritize_invalid_policy_raises_valueerror(self):
+        with pytest.raises(ValueError):
+            Prioritize(self.model, self.batch_size, self.device, "knn", "invalid")  # type: ignore
+
+    def test_prioritize_class_balance_without_labels_raises_valueerror(self):
+        with pytest.raises(ValueError):
+            emb = self.get_dataset(n=100)
+            p = Prioritize.using("knn", "class_balance", k=5)
+            p(emb)
+
+    def test_prioritize_stratified_no_scores_raises_valueerror(self):
+        with pytest.raises(ValueError):
+            emb = self.get_dataset(n=100)
+            p = Prioritize.using("kmeans_complexity", "stratified", c=5)
+            p(emb)
+
+    def test_class_balance_rerank(self):
+        classes = np.array([0, 0, 1, 1, 1, 1, 1], dtype=np.int32)
+        p = Prioritize.using("knn", "class_balance", k=5, class_label=classes, embeddings=self.get_embeddings(7))
+        # initial ordering -- easy to hard
+        inds_0 = np.arange(len(classes), dtype=np.int32)
+        # _rerank defaults to hard first with alternating classes
+        inds = p._rerank(inds_0)  # indices that rerank inds_0
+        # classes should alternate until the minority class is exhausted
+        # we're testing indices rather than classes here
+        assert all(inds_0[inds] == np.array([6, 1, 5, 0, 4, 3, 2]))
+
+    def test_stratified_rerank(self):
+        # strongly clustered and separated scores
+        scores = np.array([0.10, 0.11, 0.81, 0.82, 0.83, 0.84, 0.85], dtype=np.float64)
+        p = Prioritize.using("knn", "stratified", k=5, embeddings=self.get_embeddings(7))
+        inds_0 = np.arange(len(scores), dtype=np.int32)
+        # reversing to hard first occurs in _rerank(); this function assumes
+        #   decreasing priority
+        inds = p._stratified_rerank(scores, inds_0, num_bins=2)
+        # low score samples are in the first bin, and scores should alternate
+        assert np.all(scores[inds] == np.array([0.1, 0.81, 0.11, 0.82, 0.83, 0.84, 0.85]))
 
     @pytest.mark.parametrize(
         "emb_ref",
@@ -150,16 +208,16 @@ class TestPrioritizeSelection:
         ),
     )
     @pytest.mark.parametrize(
-        "method, method_kwargs",
+        "method, policy, method_kwargs",
         (
-            ("knn", {"k": 10}),
-            ("kmeans_distance", {"c": 100}),
-            ("kmeans_complexity", {"c": 100}),
+            ("knn", "hard_first", {"k": 10}),
+            ("kmeans_distance", "hard_first", {"c": 100}),
+            ("kmeans_complexity", "hard_first", {"c": 100}),
         ),
     )
-    def test_prioritize_using(self, method, method_kwargs, emb_ref):
+    def test_prioritize_using(self, method, policy, method_kwargs, emb_ref):
         emb_kwargs = {key: self.get_embeddings() for i, key in enumerate(["embeddings", "reference"]) if emb_ref[i]}
-        p = Prioritize.using(method, **method_kwargs, **emb_kwargs)  # type: ignore
+        p = Prioritize.using(method, policy, **method_kwargs, **emb_kwargs)  # type: ignore
         assert p._method == method
         assert (p._c == method_kwargs["c"]) if "c" in method_kwargs else (p._c is None)
         assert (p._k == method_kwargs["k"]) if "k" in method_kwargs else (p._k is None)
@@ -173,11 +231,11 @@ class TestPrioritizeSelection:
 
     def test_prioritize_using_no_embeddings_raises_valueerror(self):
         with pytest.raises(ValueError):
-            Prioritize.using("knn")
+            Prioritize.using("knn", "hard_first")
 
     def test_prioritize_using_different_embeddings_raises_valueerror(self):
         emb = self.get_embeddings(100)
-        p = Prioritize.using("knn", embeddings=emb)
+        p = Prioritize.using("knn", "easy_first", embeddings=emb)
         dataset = self.get_dataset()
         with pytest.raises(ValueError):
             p(dataset)
