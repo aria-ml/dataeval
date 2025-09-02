e1f2b29f2f41abb1b2a32d56557d244994171e9f
Andrew Weng <andrew.weng@ariacoustics.com>
2025-09-02 22:43:51 +0000

    [fix] Restore missing submodules for documention

--- src/dataeval/detectors/__init__.py
+++ src/dataeval/detectors/__init__.py
@@ -1,3 +1,7 @@
 """
 Detectors can determine if a dataset or individual images in a dataset are indicative of a specific issue.
 """
+
+from . import drift, linters, ood
+
+__all__ = ["drift", "linters", "ood"]
