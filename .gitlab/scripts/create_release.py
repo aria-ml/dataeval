08e6a51fd578c0cb7cffbd0f6bf593eab7f6fd0d
Andrew Weng <andrew.weng@ariacoustics.com>
2025-10-21 11:25:10 -0700

    Update scripts for new release strategy

--- .gitlab/scripts/create_release.py
+++ .gitlab/scripts/create_release.py
@@ -10,7 +10,9 @@ if __name__ == "__main__":
     if version_tag and payload:
         print(f"Updating jupyter cache and changelog and tagging to {version_tag}:")
         commit_id = gl.commit("main", f"Release {version_tag}", payload)["id"]
-        # gl.create_repository_branch(f"release/{version_tag}", commit_id)
+        # Create release branch with vX.X format (strip patch version)
+        major_minor = ".".join(version_tag.split(".")[:2])
+        gl.create_repository_branch(f"release/{major_minor}", commit_id)
         gl.add_tag(version_tag, commit_id, message=f"DataEval {version_tag}")
     else:
         print("No changes to commit and tag.")
