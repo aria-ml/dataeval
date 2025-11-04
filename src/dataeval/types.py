f2b539410f6b4a8aead9c3e1a33ece1ff85299a5
Andrew Weng <andrew.weng@ariacoustics.com>
2025-11-03 19:43:32 -0800

    [misc] Move SourceIndex to dataeval.types

--- src/dataeval/types.py
+++ src/dataeval/types.py
@@ -16,16 +16,20 @@ __all__ = [
 ]
 
 from collections.abc import Iterator
-from typing import Any, Protocol, TypeAlias, TypeVar
+from typing import Any, NamedTuple, Protocol, TypeAlias, TypeVar, overload, runtime_checkable
 
 from dataeval.protocols import Array
 
 DType = TypeVar("DType", covariant=True)
 
 
+@runtime_checkable
 class SequenceLike(Protocol[DType]):
     """Protocol for sequence-like objects that can be indexed and iterated."""
 
+    @overload
+    def __getitem__(self, key: int, /) -> DType: ...
+    @overload
     def __getitem__(self, key: Any, /) -> DType | SequenceLike[DType]: ...
     def __iter__(self) -> Iterator[DType]: ...
     def __len__(self) -> int: ...
@@ -52,3 +56,22 @@ ArrayND: TypeAlias = (
     | Array8D[DType]
     | Array9D[DType]
 )
+
+
+class SourceIndex(NamedTuple):
+    """
+    The indices of the source image, box and channel.
+
+    Attributes
+    ----------
+    image: int
+        Index of the source image
+    box : int | None
+        Index of the box of the source image (if applicable)
+    channel : int | None
+        Index of the channel of the source image (if applicable)
+    """
+
+    image: int
+    box: int | None
+    channel: int | None
