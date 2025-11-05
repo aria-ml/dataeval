2d5525c55e09f8937be9134dfbbf63acc2c66e51
Andrew Weng <andrew.weng@ariacoustics.com>
2025-11-05 12:04:33 -0800

    [fix] Docstring adjustment for cluster

--- src/dataeval/core/_clusterer.py
+++ src/dataeval/core/_clusterer.py
@@ -200,7 +200,7 @@ def cluster(
     Examples
     --------
     >>> output = cluster(clusterer_images)
-    >>> output.clusters
+    >>> output["clusters"]
     array([ 2,  0,  0,  0,  0,  0,  4,  0,  3,  1,  1,  0,  2,  0,  0,  0,  0,
             4,  2,  0,  0,  1,  2,  0,  1,  3,  0,  3,  3,  4,  0,  0,  3,  0,
             3, -1,  0,  0,  2,  4,  3,  4,  0,  1,  0, -1,  3,  0,  0,  0])
